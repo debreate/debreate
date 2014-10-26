0f79358999edc874cab8a3867435e8c1b6975a0a
antumdeluge <antumdeluge@61f0d44f-7c22-4d32-b124-91f4c04b28fd>
2014-10-26 07:59:18 +0000

    Create release 0.7.10.

---
+++ tags/release-0.7.10/db/dbpathctrl.py
@@ -0,0 +1,35 @@
+import wxversion
+wxversion.select(['2.6', '2.7', '2.8'])
+import wx, os
+
+PATH_DEFAULT = wx.NewId()
+PATH_WARN = wx.NewId()
+
+class PathCtrl(wx.TextCtrl):
+    def __init__(self, parent, id=wx.ID_ANY, value=wx.EmptyString, type=PATH_DEFAULT):
+        wx.TextCtrl.__init__(self, parent, id, value)
+        
+        self.type = type
+        
+        # Get the value of the textctrl so it can be restored
+        self.default = "/"
+        
+        # Make sure first character is forward slash
+        wx.EVT_KEY_UP(self, self.OnKeyUp)
+    
+    def OnKeyUp(self, event):
+        value = self.GetValue()
+        insertion_point = self.GetInsertionPoint()+1
+        if value == wx.EmptyString or value[0] != "/":
+            self.SetValue("/%s" % value)
+            self.SetInsertionPoint(insertion_point)
+        
+        # If PathCtrl is set to warn on non-existent paths, change background color to red when path
+        # doesn't exist
+        value = self.GetValue()
+        if self.type == PATH_WARN:
+            if not os.path.exists(value):
+                self.SetBackgroundColour("red")
+            else:
+                self.SetBackgroundColour("white")
+        event.Skip()
