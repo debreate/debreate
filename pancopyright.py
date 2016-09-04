79d280a53ef2281b6136ffa2b93bc4a63a8c6682
AntumDeluge <antumdeluge@gmail.com>
2016-09-04 04:29:59 -0700

    pancopyright: Add button to create a copyright template with a selection of optional licenses.

--- pancopyright.py
+++ pancopyright.py
@@ -10,9 +10,24 @@ class Panel(wx.Panel):
         
         self.debreate = parent.parent
         
+        lic_options = (
+        	'Apache-2.0', 'Artistic', 'BSD', 'GFDL', 'GFDL-1.2',
+        	'GFDL-1.3', 'GPL', 'GPL-1', 'GPL-2', 'GPL-3', 'LGPL',
+        	'LGPL-2', 'LGPL-2.1', 'LGPL-3'
+        )
+        template_btn = wx.Button(self, -1, 'Generate Template')
+        self.template_lic = wx.Choice(self, -1, choices=lic_options)
+        
+        wx.EVT_BUTTON(template_btn, -1, self.GenerateTemplate)
+        
+        template_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        template_sizer.Add(template_btn, 1)
+        template_sizer.Add(self.template_lic, 1)
+        
         self.cp_display = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         
         main_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(template_sizer, 0, wx.ALL, 5)
         main_sizer.Add(self.cp_display, 1, wx.EXPAND|wx.ALL, 5)
         
         self.SetAutoLayout(True)
@@ -31,3 +46,13 @@ class Panel(wx.Panel):
     
     def ResetAllFields(self):
         self.cp_display.Clear()
+    
+    def GenerateTemplate(self, event):
+        lic_path = '/usr/share/common-licenses/{}'.format(self.template_lic.GetString(self.template_lic.GetSelection()))
+        text = ''
+        if (not self.cp_display.IsEmpty()):
+            text = '\n{}'.format(self.cp_display.GetValue())
+            self.cp_display.Clear()
+        copyright = 'Copyright: <year> <copyright holder> <email>'
+        text = '{}\n\n{}\n{}'.format(copyright, lic_path, text)
+        self.cp_display.SetValue(text)
