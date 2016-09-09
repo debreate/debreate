3370c03a48a6ad7b4c679d1bd748067f7808717f
AntumDeluge <antumdeluge@gmail.com>
2016-09-09 00:36:07 -0700

    [update-version.py] Edit 'dbr/constants.py' instead of 'common.py'.

--- update-version.py
+++ update-version.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-FILE = open('INFO')
-info = FILE.read().split('\n')
+
+from os.path import exists
+import sys
+
+
+info_file = 'INFO'
+constants_file = 'dbr/constants.py'
+makefile_file = 'Makefile'
+
+for I in (info_file, constants_file, makefile_file):
+	if not exists(I):
+		print('ERROR: "{}" file not found, cannot update version'.format(I))
+		sys.exit(1)
+
+FILE = open(info_file)
+info_data = FILE.read().split('\n')
 FILE.close()
 
 keys = {}
 
-for l in info:
+for l in info_data:
 	if '=' in l:
 		key = l.split('=')[0]
 		value = l.split('=')[1]
@@ -20,37 +34,37 @@ ver_rel = ver.split('.')[2]
 release = keys['RELEASE']
 
 
-# Update version numbers in 'common.py'
-FILE = open('common.py', 'r')
-common = FILE.read().split('\n')
+# Update version numbers in 'constants.py'
+FILE = open(constants_file, 'r')
+constants_data = FILE.read().split('\n')
 FILE.close()
 
-for l in common:
-	index = common.index(l)
-	if ('ver_maj =' in l):
-		common[index] = 'ver_maj = {}'.format(ver_maj)
-	elif ('ver_min =' in l):
-		common[index] = 'ver_min = {}'.format(ver_min)
-	elif ('ver_rel =' in l):
-		common[index] = 'ver_rel = {}'.format(ver_rel)
+for l in constants_data:
+	index = constants_data.index(l)
+	if ('VER_MAJ =' in l):
+		constants_data[index] = 'VER_MAJ = {}'.format(ver_maj)
+	elif ('VER_MIN =' in l):
+		constants_data[index] = 'VER_MIN = {}'.format(ver_min)
+	elif ('VER_REL =' in l):
+		constants_data[index] = 'VER_REL = {}'.format(ver_rel)
 	elif ('RELEASE =' in l):
-		common[index] = 'RELEASE = {}'.format(release)
+		constants_data[index] = 'RELEASE = {}'.format(release)
 
-FILE = open('common.py', 'w')
-FILE.write('\n'.join(common))
+FILE = open(constants_file, 'w')
+FILE.write('\n'.join(constants_data))
 FILE.close()
 
 
 # Update version numbers in 'Makefile'
-FILE = open('Makefile', 'r')
-makefile = FILE.read().split('\n')
+FILE = open(makefile_file, 'r')
+makefile_data = FILE.read().split('\n')
 FILE.close()
 
-for l in makefile:
-	index = makefile.index(l)
+for l in makefile_data:
+	index = makefile_data.index(l)
 	if ('VERSION =' in l or 'VERSION=' in l):
-		makefile[index] = 'VERSION = {}'.format(ver)
+		makefile_data[index] = 'VERSION = {}'.format(ver)
 
-FILE = open('Makefile', 'w')
-FILE.write('\n'.join(makefile))
+FILE = open(makefile_file, 'w')
+FILE.write('\n'.join(makefile_data))
 FILE.close()
