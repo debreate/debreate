0c5df359e668b1227a196d9dab54fa77aa90c00c
AntumDeluge <antumdeluge@gmail.com>
2016-09-16 14:48:02 -0700

    dbr.__init__: Only import required functions & classes from sub-modules.

--- dbr/__init__.py
+++ dbr/__init__.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from about import AboutDialog
-from buttons import *
+from buttons import \
+    ButtonAdd, ButtonBrowse, ButtonBrowse64, ButtonBuild, ButtonBuild64, \
+    ButtonCancel, ButtonClear, ButtonConfirm, ButtonDel, ButtonImport, \
+    ButtonNext, ButtonPipe, ButtonPrev, ButtonPreview, ButtonPreview64, \
+    ButtonQuestion64, ButtonSave, ButtonSave64
 from charctrl import CharCtrl
-from constants import *
-from functions import *
+from constants import \
+    APP_NAME, VER_MAJ, VER_MIN, VER_REL, VERSION, VERSION_STRING, \
+    HOMEPAGE, gh_project, sf_project
+from functions import \
+    GetCurrentVersion, FieldEnabled, RunSudo
 from language import GT
 from message import MessageDialog
 from pathctrl import PathCtrl, PATH_DEFAULT, PATH_WARN
 from wizard import Wizard
+
+
