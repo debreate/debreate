3d9fc7add463381536507e51d7d0962d35861742
AntumDeluge <antumdeluge@gmail.com>
2016-09-16 15:35:25 -0700

    main: Remove reference to GPL licensing.

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
 # This script is no longer executable, use launch.py
 # -*- coding: utf-8 -*-
 
-"""
-    Debreate - Debian Package Builder v0.7.3
-    Copyright (C) 2010  Jordan Irwin <antumdeluge@gmail.com>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
-
-# Standard Python modules
 import os, shutil, subprocess
 from urllib2 import URLError, HTTPError
 import webbrowser
