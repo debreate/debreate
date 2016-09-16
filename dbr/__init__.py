076b1ec880ecd9028e02bd5671080df69ef0bfb9
AntumDeluge <antumdeluge@gmail.com>
2016-09-16 14:12:07 -0700

    dbr.__init__: Import 'GT' from 'language' module.

--- dbr/__init__.py
+++ dbr/__init__.py
@@ -2,9 +2,10 @@
 
 from about import AboutDialog
 from buttons import *
+from charctrl import CharCtrl
 from constants import *
 from functions import *
-from wizard import Wizard
-from charctrl import CharCtrl
-from pathctrl import PathCtrl, PATH_DEFAULT, PATH_WARN
+from language import GT
 from message import MessageDialog
+from pathctrl import PathCtrl, PATH_DEFAULT, PATH_WARN
+from wizard import Wizard
