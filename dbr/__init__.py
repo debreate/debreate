e9a7eafcb29fb4a71955d59b0a13ae06253cd8ea
AntumDeluge <antumdeluge@gmail.com>
2016-09-16 15:49:19 -0700

    dbr.__init__: Import more constants from 'dbr.constants'.

--- dbr/__init__.py
+++ dbr/__init__.py
@@ -10,7 +10,8 @@ from dbr.buttons import \
 from dbr.charctrl import CharCtrl
 from dbr.constants import \
     APP_NAME, VER_MAJ, VER_MIN, VER_REL, VERSION, VERSION_STRING, \
-    HOMEPAGE, gh_project, sf_project
+    HOMEPAGE, gh_project, sf_project, \
+    PY_VER_MAJ, PY_VER_MIN, PY_VER_REL, PY_VER_STRING, WX_VER_STRING
 from dbr.functions import \
     GetCurrentVersion, FieldEnabled, RunSudo
 from dbr.language import GT
