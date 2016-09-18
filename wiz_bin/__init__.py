6b36329d6933b1412ac9bd6ad98073aa663c6019
AntumDeluge <antumdeluge@gmail.com>
2016-09-18 00:03:58 -0700

    Move modules from 'pages' package to 'wiz_bin'.

---
+++ wiz_bin/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from wiz_bin.build import Panel as PageBuild, QuickBuild
+from wiz_bin.clog import Panel as PageChangelog
+from wiz_bin.control import Panel as PageControl
+from wiz_bin.copyright import Panel as PageCopyright
+from wiz_bin.depends import Panel as PageDepends
+from wiz_bin.files import Panel as PageFiles
+from wiz_bin.info import Panel as PageInfo
+from wiz_bin.menu import Panel as PageMenu
+from wiz_bin.scripts import Panel as PageScripts
