72a85bf2834afeaaf8745b9b96c91e56b3c47544
AntumDeluge <antumdeluge@gmail.com>
2016-09-18 00:22:16 -0700

    wiz_src.control: Create 'Panel' class & import as 'wiz_src.PageControl'.

--- wiz_src/control.py
+++ wiz_src/control.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 
+
+from wx import \
+    BoxSizer as wxBoxSizer, \
+    NewId as wxNewId, \
+    Panel as wxPanel
+from wx import \
+    VERTICAL as wxVERTICAL
+
+
+
+ID = wxNewId()
+
+
+class Panel(wxPanel):
+    def __init__(self, parent, name=_('Control')):
+        wxPanel.__init__(self, parent, ID, name=name)
+        
+        page_layout = wxBoxSizer(wxVERTICAL)
