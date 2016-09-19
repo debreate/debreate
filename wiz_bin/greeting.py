64466ff21bcb58fc609a892ec03ea4938280d18c
Hugo Posnic <hugo.posnic@protonmail.com>
2016-09-19 20:55:40 +0200

    Remove a trailing whitespace

--- wiz_bin/greeting.py
+++ wiz_bin/greeting.py
@@ -28,7 +28,7 @@ class Panel(wxPanel):
         wxPanel.__init__(self, parent, id, name=GT('Information'))
 
         self.parent = parent # 3rd level) Allows executing 2st level methods
-        
+
         # Mode Information
         m1 = GT('Welcome to Debreate!')
         m2 = GT('Debreate aids in building packages for installation on Debian based systems. Use the arrows located in the top-right corner or the "Page" menu to navigate through the program. For some information on Debian packages use the reference links in the "Help" menu.')
