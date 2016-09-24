78b6b29fec32a1c329f495f023b0059a7371bd55
AntumDeluge <antumdeluge@gmail.com>
2016-09-23 17:10:05 -0700

    dbr.font: Create font globals/constants.

---
+++ dbr/font.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+## \package dbr.font
+
+
+# System modules
+import wx
+
+
+SIZE_SM = 6
+SIZE_MD = 8
+SIZE_LG = 10
+
+# Monospaced
+MONOSPACED_SM = wx.Font(SIZE_SM, wx.FONTFAMILY_TELETYPE,
+                             wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+MONOSPACED_MD = wx.Font(SIZE_MD, wx.FONTFAMILY_TELETYPE,
+                             wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+MONOSPACED_LG = wx.Font(SIZE_LG, wx.FONTFAMILY_TELETYPE,
+                             wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
