db757ecd4dd464fcd40c79815987eb52238fcc6b
AntumDeluge <antumdeluge@gmail.com>
2016-09-28 01:39:24 -0700

    scripts/update-version: Match only lines that begin with search delim.

--- scripts/update-version.py
+++ scripts/update-version.py
@@ -12,9 +12,9 @@ makefile_file = 'Makefile'
 doxygen_file = 'docs/Doxyfile'
 
 for I in (info_file, constants_file, makefile_file, doxygen_file):
-	if not exists(I):
-		print('ERROR: "{}" file not found, cannot update version'.format(I))
-		sys.exit(1)
+    if not exists(I):
+        print('ERROR: "{}" file not found, cannot update version'.format(I))
+        sys.exit(1)
 
 FILE = open(info_file)
 info_data = FILE.read().split('\n')
@@ -23,10 +23,10 @@ FILE.close()
 keys = {}
 
 for l in info_data:
-	if '=' in l:
-		key = l.split('=')[0]
-		value = l.split('=')[1]
-		keys[key] = value
+    if '=' in l:
+        key = l.split('=')[0]
+        value = l.split('=')[1]
+        keys[key] = value
 
 VERSION = keys['VERSION']
 VER_MAJ = VERSION.split('.')[0]
@@ -40,20 +40,32 @@ FILE = open(constants_file, 'r')
 constants_data = FILE.read().split('\n')
 FILE.close()
 
-for l in constants_data:
-	index = constants_data.index(l)
-	if ('VER_MAJ =' in l):
-		constants_data[index] = 'VER_MAJ = {}'.format(VER_MAJ)
-	elif ('VER_MIN =' in l):
-		constants_data[index] = 'VER_MIN = {}'.format(VER_MIN)
-	elif ('VER_REL =' in l):
-		constants_data[index] = 'VER_REL = {}'.format(VER_REL)
-	elif ('RELEASE =' in l):
-		constants_data[index] = 'RELEASE = {}'.format(RELEASE)
+file_changed = False
 
-FILE = open(constants_file, 'w')
-FILE.write('\n'.join(constants_data))
-FILE.close()
+for l in constants_data:
+    index = constants_data.index(l)
+    line_start = l[:9]
+    
+    if (line_start == 'VER_MAJ ='):
+        constants_data[index] = 'VER_MAJ = {}'.format(VER_MAJ)
+        file_changed = True
+    
+    elif (line_start == 'VER_MIN ='):
+        constants_data[index] = 'VER_MIN = {}'.format(VER_MIN)
+        file_changed = True
+    
+    elif (line_start == 'VER_REL ='):
+        constants_data[index] = 'VER_REL = {}'.format(VER_REL)
+        file_changed = True
+    
+    elif (line_start == 'RELEASE ='):
+        constants_data[index] = 'RELEASE = {}'.format(RELEASE)
+        file_changed = True
+
+if file_changed:
+    FILE = open(constants_file, 'w')
+    FILE.write('\n'.join(constants_data))
+    FILE.close()
 
 
 # Update version numbers in 'Makefile'
@@ -61,14 +73,20 @@ FILE = open(makefile_file, 'r')
 makefile_data = FILE.read().split('\n')
 FILE.close()
 
-for l in makefile_data:
-	index = makefile_data.index(l)
-	if ('VERSION =' in l or 'VERSION=' in l):
-		makefile_data[index] = 'VERSION = {}'.format(VERSION)
+file_changed = False
 
-FILE = open(makefile_file, 'w')
-FILE.write('\n'.join(makefile_data))
-FILE.close()
+for l in makefile_data:
+    index = makefile_data.index(l)
+    line_start = l[:9]
+    
+    if line_start == 'VERSION =':
+        makefile_data[index] = 'VERSION = {}'.format(VERSION)
+        file_changed = True
+
+if file_changed:
+    FILE = open(makefile_file, 'w')
+    FILE.write('\n'.join(makefile_data))
+    FILE.close()
 
 
 # Update version in 'Doxyfile'
