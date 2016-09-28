3b9fc9ee59a636e3aa01d1dfb1ca627b4bbb6f48
AntumDeluge <antumdeluge@gmail.com>
2016-09-28 01:14:25 -0700

    scripts/update-version: Update app version in 'docs/Doxyfile'.

--- scripts/update-version.py
+++ scripts/update-version.py
@@ -9,8 +9,9 @@ import sys
 info_file = 'INFO'
 constants_file = 'dbr/constants.py'
 makefile_file = 'Makefile'
+doxygen_file = 'docs/Doxyfile'
 
-for I in (info_file, constants_file, makefile_file):
+for I in (info_file, constants_file, makefile_file, doxygen_file):
 	if not exists(I):
 		print('ERROR: "{}" file not found, cannot update version'.format(I))
 		sys.exit(1)
@@ -27,11 +28,11 @@ for l in info_data:
 		value = l.split('=')[1]
 		keys[key] = value
 
-ver = keys['VERSION']
-ver_maj = ver.split('.')[0]
-ver_min = ver.split('.')[1]
-ver_rel = ver.split('.')[2]
-release = keys['RELEASE']
+VERSION = keys['VERSION']
+VER_MAJ = VERSION.split('.')[0]
+VER_MIN = VERSION.split('.')[1]
+VER_REL = VERSION.split('.')[2]
+RELEASE = keys['RELEASE']
 
 
 # Update version numbers in 'constants.py'
@@ -42,13 +43,13 @@ FILE.close()
 for l in constants_data:
 	index = constants_data.index(l)
 	if ('VER_MAJ =' in l):
-		constants_data[index] = 'VER_MAJ = {}'.format(ver_maj)
+		constants_data[index] = 'VER_MAJ = {}'.format(VER_MAJ)
 	elif ('VER_MIN =' in l):
-		constants_data[index] = 'VER_MIN = {}'.format(ver_min)
+		constants_data[index] = 'VER_MIN = {}'.format(VER_MIN)
 	elif ('VER_REL =' in l):
-		constants_data[index] = 'VER_REL = {}'.format(ver_rel)
+		constants_data[index] = 'VER_REL = {}'.format(VER_REL)
 	elif ('RELEASE =' in l):
-		constants_data[index] = 'RELEASE = {}'.format(release)
+		constants_data[index] = 'RELEASE = {}'.format(RELEASE)
 
 FILE = open(constants_file, 'w')
 FILE.write('\n'.join(constants_data))
@@ -63,8 +64,27 @@ FILE.close()
 for l in makefile_data:
 	index = makefile_data.index(l)
 	if ('VERSION =' in l or 'VERSION=' in l):
-		makefile_data[index] = 'VERSION = {}'.format(ver)
+		makefile_data[index] = 'VERSION = {}'.format(VERSION)
 
 FILE = open(makefile_file, 'w')
 FILE.write('\n'.join(makefile_data))
 FILE.close()
+
+
+# Update version in 'Doxyfile'
+FILE = open(doxygen_file, 'r')
+doxygen_data = FILE.read().split('\n')
+FILE.close()
+
+file_changed = False
+
+for l in doxygen_data:
+    index = doxygen_data.index(l)
+    if 'PROJECT_NUMBER         =' in l:
+        doxygen_data[index] = 'PROJECT_NUMBER         = {}'.format(VERSION)
+        file_changed = True
+
+if file_changed:
+    FILE = open(doxygen_file, 'w')
+    FILE.write('\n'.join(doxygen_data))
+    FILE.close()
