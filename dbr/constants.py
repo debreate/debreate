b88a77682d296ff2cf54f7c337a577fc550c13be
AntumDeluge <antumdeluge@gmail.com>
2016-09-28 01:19:39 -0700

    dbr.constans: Fix overwritten variables 'PY_VER_*'.
    NOTE: Need to fix 'update-version' script.

--- dbr/constants.py
+++ dbr/constants.py
@@ -64,9 +64,9 @@ PROJECT_FILENAME_SUFFIX = u'dbpz'
 
 # *** Python Info *** #
 
-VER_MAJ = 0
-VER_MIN = 8
-VER_REL = 0
+PY_VER_MAJ = sys.version_info[0]
+PY_VER_MIN = sys.version_info[1]
+PY_VER_REL = sys.version_info[2]
 PY_VER_STRING = u'{}.{}.{}'.format(PY_VER_MAJ, PY_VER_MIN, PY_VER_REL)
 
 
