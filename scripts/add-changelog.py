10b03a19af50043f9d89fd6f5b31bc9ae65d9245
AntumDeluge <antumdeluge@gmail.com>
2016-10-03 18:15:34 -0700

    Utility scripts: Improve add-changelog
    Import from globals script.
    Optionally add filename argument to import changes from file.

--- scripts/add-changelog.py
+++ scripts/add-changelog.py
@@ -10,65 +10,98 @@
 #     - Edit 'docs/changelog' to add new changes
 #     - For Debian builds:
 #       - Copy 'docs/changelog' to 'debian/changelog'
+#
+#    If a filename is used as the first argument, the
+#    script will copy any lines beginning with an alpha-
+#    numeric character & add them to the changelog
+#    output file.
 
-import os, sys, time
+import os, sys, time, errno
+from scripts_globals import file_CHANGELOG, GetInfoValue
 
-cl_file = u'docs/changelog'
-info_file = u'INFO'
 
-if not os.path.isfile(info_file):
-    print(u'ERROR: Cannot find file "{}" for reading'.format(info_file))
-    sys.exit(1)
+CHANGES = None
 
-info_data = open(info_file, u'r')
-info_lines = info_data.read().split(u'\n')
-info_data.close()
+# Writes information from changes_file to changelog output
+def AddChanges(changes_file):
+    if not os.path.isfile(changes_file):
+        print('ERROR: File does not exist: {}'.format(changes_file))
+        sys.exit(errno.ENOENT)
+    
+    FILE = open(changes_file)
+    changes_data = FILE.read().split('\n')
+    FILE.close()
+    
+    changes_lines = []
+    for LI in changes_data:
+        if len(LI):
+            # Only recognize lines that begin with an alphabetic letter or number
+            if LI[0].isalnum():
+                changes_lines.append(LI)
+    
+    if len(changes_lines):
+        return changes_lines
+    
+    return None
 
-info = {}
-for L in info_lines:
-    if len(L):
-        # Skip lines that begin with hashtag or whitespace
-        if L[0] not in (u'#', u' '):
-            if u'=' in L:
-                key = L.split(u'=')[0]
-                value = L.split(u'=')[1]
-                info[key] = value
 
 # Package details
-package = info[u'NAME'].lower()
-new_version = info[u'VERSION']
+package = GetInfoValue('NAME').lower()
+new_version = GetInfoValue('VERSION')
 
 # Packager details
-packager = info[u'AUTHOR']
-email = info[u'EMAIL']
+packager = GetInfoValue('AUTHOR')
+email = GetInfoValue('EMAIL')
 
 # Distribution details
-dist = info[u'DIST']
-urgency = info[u'URGENCY']
+dist = GetInfoValue('DIST')
+urgency = GetInfoValue('URGENCY')
 
-date_string = unicode(time.strftime(u'%a, %d %b %Y'))
-time_string = unicode(time.strftime(u'%T %z'))
-
-entry_string = u'{} ({}) {}; urgency={}\n\n  *\n\n'.format(package, new_version, dist, urgency)
-entry_string += u' -- {} <{}>  {} {}'.format(packager, email, date_string, time_string)
+date_string = unicode(time.strftime('%a, %d %b %Y'))
+time_string = unicode(time.strftime('%T %z'))
 
 
-new_log = not os.path.isfile(cl_file)
+# Check if we are importing changes from a text file
+if len(sys.argv) > 1:
+    CHANGES = AddChanges(sys.argv[1])
+
+if CHANGES != None and len(CHANGES):
+    for LI in CHANGES:
+        l_index = CHANGES.index(LI)
+        if l_index == 0:
+            CHANGES[l_index] = '  * {}'.format(CHANGES[l_index])
+        else:
+            CHANGES[l_index] = '    {}'.format(CHANGES[l_index])
+    
+    CHANGES = '\n'.join(CHANGES)
+else:
+    CHANGES = '  *'
+
+entry_string = '{} ({}) {}; urgency={}\n\n{}\n\n'.format(package, new_version, dist, urgency, CHANGES)
+entry_string += ' -- {} <{}>  {} {}'.format(packager, email, date_string, time_string)
+
+new_log = not os.path.isfile(file_CHANGELOG)
 
 if not new_log:
-    cl_data = open(cl_file, u'r')
+    cl_data = open(file_CHANGELOG, 'r')
     cl_text = cl_data.read()
     cl_data.close()
     
     # Check if log is empty
-    new_log = (u''.join(u''.join(cl_text.split(u' ')).split(u'\n')) == u'')
+    new_log = (''.join(''.join(cl_text.split(' ')).split('\n')) == '')
 
 if new_log:
-    cl_text = u'{}\n'.format(entry_string)
+    cl_text = '{}\n'.format(entry_string)
 
 else:
-    cl_text = u'{}\n\n\n{}'.format(entry_string, cl_text)
+    log_lines = cl_text.split('\n')
+    for LI in log_lines:
+        if 'debreate ({})'.format(new_version) in LI:
+            print('There is already an entry for version {}, exiting ...'.format(new_version))
+            sys.exit(0)
+    
+    cl_text = '{}\n\n\n{}'.format(entry_string, cl_text)
 
-cl_data = open(cl_file, u'w')
+cl_data = open(file_CHANGELOG, 'w')
 cl_data.write(cl_text)
 cl_data.close()
