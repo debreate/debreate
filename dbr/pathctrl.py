99301932a04f61c8d6b539e6805c7e0b167e5b28
AntumDeluge <antumdeluge@gmail.com>
2016-10-06 16:40:57 -0700

    dbr.pathctrl: Imporove class
    Add 'Reset', 'SetDefaultValue', & 'SetPathAvailable' methods.
    'SetPathAvailable' checks if path exists & marks control with red if
    not.

--- dbr/pathctrl.py
+++ dbr/pathctrl.py
@@ -5,6 +5,8 @@ import wx, os
 PATH_DEFAULT = wx.NewId()
 PATH_WARN = wx.NewId()
 
+
+# FIXME: Use boolean value instead of type
 class PathCtrl(wx.TextCtrl):
     def __init__(self, parent, id=wx.ID_ANY, value=wx.EmptyString, type=PATH_DEFAULT):
         wx.TextCtrl.__init__(self, parent, id, value)
@@ -14,8 +16,23 @@ class PathCtrl(wx.TextCtrl):
         # Get the value of the textctrl so it can be restored
         self.default = value
         
+        # For restoring color of text area
+        self.bg_color = self.GetBackgroundColour()
+        
         # Make sure first character is forward slash
         wx.EVT_KEY_UP(self, self.OnKeyUp)
+        
+        # Check if path is available on construction
+        if self.type == PATH_WARN:
+            self.SetPathAvailable()
+    
+    
+    def SetPathAvailable(self):
+        if os.path.isdir(self.GetValue()):
+            self.SetBackgroundColour(self.bg_color)
+            return
+        
+        self.SetBackgroundColour(u'red')
     
     def OnKeyUp(self, event):
         value = self.GetValue()
@@ -28,8 +45,17 @@ class PathCtrl(wx.TextCtrl):
         # doesn't exist
         value = self.GetValue()
         if self.type == PATH_WARN:
-            if not os.path.exists(value):
-                self.SetBackgroundColour(u'red')
-            else:
-                self.SetBackgroundColour(u'white')
+            self.SetPathAvailable()
+        
         event.Skip()
+    
+    
+    def Reset(self):
+        self.SetValue(self.default)
+        
+        if self.type == PATH_WARN:
+            self.SetPathAvailable()
+    
+    
+    def SetDefaultValue(self, value):
+        self.default = value
