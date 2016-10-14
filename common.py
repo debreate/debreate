a291f0ed0e3cb2947c2ef675b3af7644b1dc0715
AntumDeluge <antumdeluge@gmail.com>
2016-10-14 08:03:51 -0700

    Increment dev verson & update changelog

--- common.py
+++ common.py
@@ -26,7 +26,7 @@ debreate_version = u'{}.{}.{}'.format(ver_maj, ver_min, ver_rel)
 
 if not RELEASE:
     # Increment this for every development release
-    ver_dev = 1
+    ver_dev = 2
     debreate_version = u'{}-dev{}'.format(debreate_version, ver_dev)
 
 db_version = (ver_maj, ver_min, ver_rel)
