57f6f1514a3478538ab82f1fffae9c26cad137c5
AntumDeluge <antumdeluge@gmail.com>
2016-10-16 23:40:09 -0700

    Move some globals into globals.bitmaps

---
+++ globals/bitmaps.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+## \package <package>
+
+# MIT licensing
+# See: docs/LICENSE.txt
+
+
+# Local modules
+from globals.paths import PATH_app
+
+
+ICON_ERROR = u'{}/bitmaps/error64.png'.format(PATH_app)
+ICON_INFORMATION = u'{}/bitmaps/question64.png'.format(PATH_app)
