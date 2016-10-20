957c9adada5389342e306d34ef2aa5ce15465c7c
AntumDeluge <antumdeluge@gmail.com>
2016-10-19 17:28:44 -0700

    globals.colors: Define colors for some controls

---
+++ globals/colors.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+## \package globals.colors
+
+# MIT licensing
+# See: docs/LICENSE.txt
+
+
+from wx import Colour as Color
+
+
+# Yellow
+COLOR_tooltip = Color(255, 255, 0)
