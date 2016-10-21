804800d2c3212ed4f55102e499bcae8d26c7b0c6
AntumDeluge <antumdeluge@gmail.com>
2016-10-21 12:15:57 -0700

    globals.bitmaps: Add global bitmap for help button

--- globals/bitmaps.py
+++ globals/bitmaps.py
@@ -6,9 +6,12 @@
 # See: docs/LICENSE.txt
 
 
-# Local modules
+import wx
+
 from globals.paths import PATH_app
 
 
 ICON_ERROR = u'{}/bitmaps/error64.png'.format(PATH_app)
 ICON_INFORMATION = u'{}/bitmaps/question64.png'.format(PATH_app)
+
+BUTTON_HELP = wx.Bitmap(u'{}/bitmaps/question64.png'.format(PATH_app))
