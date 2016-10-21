4c092b53723dd0fda9e17d62a234ccb42b135ccc
AntumDeluge <antumdeluge@gmail.com>
2016-10-21 12:18:57 -0700

    Use 32x32 bitmap for page help button

--- globals/bitmaps.py
+++ globals/bitmaps.py
@@ -14,4 +14,4 @@ from globals.paths import PATH_app
 ICON_ERROR = u'{}/bitmaps/error64.png'.format(PATH_app)
 ICON_INFORMATION = u'{}/bitmaps/question64.png'.format(PATH_app)
 
-BUTTON_HELP = wx.Bitmap(u'{}/bitmaps/question64.png'.format(PATH_app))
+BUTTON_HELP = wx.Bitmap(u'{}/bitmaps/question32.png'.format(PATH_app))
