90613f50513fce8bd235cc17d495117f4543a3cd
AntumDeluge <antumdeluge@gmail.com>
2016-10-31 18:08:26 -0700

    dbr.custom: Merge fixed first run dialog from 0.7 branch

--- dbr/custom.py
+++ dbr/custom.py
@@ -33,20 +33,32 @@ class FirstRun(wx.Dialog):
     def __init__(self):
         wx.Dialog.__init__(self, None, wx.ID_ANY, GT(u'Debreate First Run'), size=(450,300))
         
-        m1 = GT(u'Thank you for using Debreate.')
-        m2 = GT(u'This message only displays on the first run, or if the configuration file becomes corrupted. The default configuration file will now be created. To delete this file, type the following command in a terminal:')
+        m2 = GT(u'This message only displays on the first run, or if\nthe configuration file becomes corrupted.')
+        m3 = GT(u'The default configuration file will now be created.')
+        m4 = GT(u'To delete this file, type the following command in a\nterminal:')
         
         # "OK" button sets to True
         #self.OK = False
+        
+        message1 = GT(u'Thank you for using Debreate.')
+        message1 = u'{}\n\n{}'.format(message1, m2)
+        
+        message2 = m3
+        message2 = u'{}\n{}'.format(message2, m4)
         
         # Set the titlebar icon
         self.SetIcon(wx.Icon(u'{}/bitmaps/debreate64.png'.format(PATH_app), wx.BITMAP_TYPE_PNG))
         
         # Display a message to create a config file
-        self.message = wx.StaticText(self)
-        self.message.SetLabel(u'{}\n\n\
-{}\n\n\
-rm -r ~/.config/debreate'.format(m1, m2))
+        text1 = wx.StaticText(self, label=message1)
+        text2 = wx.StaticText(self, label=message2)
+        
+        rm_cmd = wx.StaticText(self, label=u'rm -f ~/.config/debreate/config')
+        
+        layout_V1 = wx.BoxSizer(wx.VERTICAL)
+        layout_V1.Add(text1, 1)
+        layout_V1.Add(text2, 1, wx.TOP, 15)
+        layout_V1.Add(rm_cmd, 0, wx.TOP, 10)
         
         # Show the Debreate icon
         dbicon = wx.Bitmap(u'{}/bitmaps/debreate64.png'.format(PATH_app), wx.BITMAP_TYPE_PNG)
@@ -61,7 +73,7 @@ rm -r ~/.config/debreate'.format(m1, m2))
         border_box.AddSpacer(10)
         border_box.Add(icon, 0, wx.ALIGN_CENTER)
         border_box.AddSpacer(10)
-        border_box.Add(self.message, 1, wx.ALIGN_CENTER)
+        border_box.Add(layout_V1, 1, wx.ALIGN_CENTER)
         
         # Set Layout
         sizer = wx.BoxSizer(wx.VERTICAL)
