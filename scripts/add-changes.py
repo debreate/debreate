dd4eb8137de82253255eaac15242ed97ce9c3107
AntumDeluge <antumdeluge@gmail.com>
2016-11-03 17:38:33 -0700

    Revert to old changelog format

--- scripts/add-changes.py
+++ scripts/add-changes.py
@@ -3,13 +3,11 @@
 
 # HOWTO:
 #   To add a changelog entry:
-#     - Edit the INFO file in the root directory
-#     - Change the values of AUTHOR & EMAIL
 #     - Run this script (from any location)
 #       - A new entry will be written to 'docs/changelog'
 #     - Edit 'docs/changelog' to add new changes
 #     - For Debian builds:
-#       - Copy 'docs/changelog' to 'debian/changelog'
+#       - Run 'scripts/update-debian-changelog.py'
 #
 #    If a filename is used as the first argument, the
 #    script will copy any lines beginning with an alpha-
@@ -17,7 +15,9 @@
 #    output file.
 
 import os, sys, time, errno
-from scripts_globals import file_CHANGELOG, GetInfoValue
+
+from scripts_globals import GetInfoValue
+from scripts_globals import file_CHANGELOG
 
 
 CHANGES = None
@@ -38,6 +38,10 @@ def AddChanges(changes_file):
             # Only recognize lines that begin with an alphabetic letter or number
             if LI[0].isalnum():
                 changes_lines.append(LI)
+            
+            # These lines are indented
+            elif LI.strip(' ') and LI.strip(' ')[0] in ('-', '*', '\t'):
+                changes_lines.append(LI.strip(' '))
     
     if len(changes_lines):
         return changes_lines
@@ -45,20 +49,14 @@ def AddChanges(changes_file):
     return None
 
 
-# Package details
-package = GetInfoValue('NAME').lower()
+# Version details
 new_version = GetInfoValue('VERSION')
-
-# Packager details
-packager = GetInfoValue('AUTHOR')
-email = GetInfoValue('EMAIL')
-
-# Distribution details
-dist = GetInfoValue('DIST')
-urgency = GetInfoValue('URGENCY')
-
-date_string = unicode(time.strftime('%a, %d %b %Y'))
-time_string = unicode(time.strftime('%T %z'))
+dev_version = GetInfoValue('VERSION_dev')
+if dev_version.isdigit():
+    dev_version = int(dev_version)
+    
+    if dev_version:
+        new_version = '{}-dev{}'.format(new_version, dev_version)
 
 
 # Check if we are importing changes from a text file
@@ -68,17 +66,20 @@ if len(sys.argv) > 1:
 if CHANGES != None and len(CHANGES):
     for LI in CHANGES:
         l_index = CHANGES.index(LI)
-        if l_index == 0:
-            CHANGES[l_index] = '  * {}'.format(CHANGES[l_index])
-        else:
-            CHANGES[l_index] = '    {}'.format(CHANGES[l_index])
+        
+        # These lines are indented
+        if LI[0] in ('-', '*', '\t'):
+            # Remove delimter & strip whitespace
+            CHANGES[l_index] = '  - {}'.format(LI[1:].strip(' '))
+            continue
+        
+        CHANGES[l_index] = '- {}'.format(CHANGES[l_index])
     
     CHANGES = '\n'.join(CHANGES)
 else:
-    CHANGES = '  *'
+    CHANGES = '- <add changes here>'
 
-entry_string = '{} ({}) {}; urgency={}\n\n{}\n\n'.format(package, new_version, dist, urgency, CHANGES)
-entry_string += ' -- {} <{}>  {} {}'.format(packager, email, date_string, time_string)
+entry_string = '{}\n{}'.format(new_version, CHANGES)
 
 new_log = not os.path.isfile(file_CHANGELOG)
 
@@ -96,11 +97,11 @@ if new_log:
 else:
     log_lines = cl_text.split('\n')
     for LI in log_lines:
-        if 'debreate ({})'.format(new_version) in LI:
+        if LI.startswith(new_version):
             print('There is already an entry for version {}, exiting ...'.format(new_version))
             sys.exit(0)
     
-    cl_text = '{}\n\n\n{}'.format(entry_string, cl_text)
+    cl_text = '{}\n\n{}'.format(entry_string, cl_text)
 
 cl_data = open(file_CHANGELOG, 'w')
 cl_data.write(cl_text)
