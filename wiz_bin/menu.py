b8e2afb6fc4212ec94a89bcbb305d57cfa3e375c
AntumDeluge <antumdeluge@gmail.com>
2016-11-16 16:49:57 -0800

    wiz_bin.menu: Add GetOutputFilename method
    Sets the filename prefix for the launcher during build.

--- wiz_bin/menu.py
+++ wiz_bin/menu.py
@@ -363,6 +363,17 @@ class Panel(wx.ScrolledWindow):
         return u'\n'.join(desktop_list)
     
     
+    ## Retrieves the filename to be used for the menu launcher
+    def GetOutputFilename(self):
+        if not self.chk_filename.GetValue():
+            filename = self.input_filename.GetValue().replace(u' ', u'_')
+            if not TextIsEmpty(filename):
+                return filename
+        
+        return self.name_input.GetValue().replace(u' ', u'_')
+            
+    
+    
     def SetCategory(self, event):
         try:
             id = event.GetKeyCode()
