0becb2e287688072b093aeada9ac0f7c99895516
AntumDeluge <antumdeluge@gmail.com>
2016-11-23 10:14:24 -0800

    Merge globals.characters & globals.tooltips from unstable branch

---
+++ globals/characters.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+## \package globals.characters
+#  
+#  Variables defining special Unicode/ASCII characters
+
+# MIT licensing
+# See: docs/LICENSE.txt
+
+
+ARROW_RIGHT = u'➜'
