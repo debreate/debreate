f179744a0e8e4be03dc94428c129141b1069dcfb
AntumDeluge <antumdeluge@gmail.com>
2016-11-24 17:10:34 -0800

    dbr.wizard: Code cleanup
    Use global IDs from globals.ident.
    Do not redefind built-in types 'id', 'list', & 'tuple'.
    Use 'isinstance' function instead of comparing types manually.
    Set 'event' argument to 'None' by default in methods.
    Remove some old commented-out lines.
    Fix some whitespace.
    Don't explicitly declare object instance IDs where unnecessary.
    Sort methods alphabetically.

--- dbr/wizard.py
+++ dbr/wizard.py
@@ -2,19 +2,21 @@
 
 # Wizard class for Debreate
 
+
 import wx
 from wx.lib.newevent import NewCommandEvent
 
-from dbr.buttons import ButtonNext
-from dbr.buttons import ButtonPrev
+from dbr.buttons    import ButtonNext
+from dbr.buttons    import ButtonPrev
+from dbr.language   import GT
+from globals.ident  import ID_NEXT
+from globals.ident  import ID_PREV
 
 
-ID_PREV = wx.NewId()
-ID_NEXT = wx.NewId()
-
+## TODO: Doxygen
 class Wizard(wx.Panel):
-    def __init__(self, parent, id=wx.ID_ANY, page_list=None):
-        wx.Panel.__init__(self, parent, id, page_list)
+    def __init__(self, parent, ID=wx.ID_ANY, page_list=None):
+        wx.Panel.__init__(self, parent, ID, page_list)
         
         # List of pages available in the wizard
         self.pages = []
@@ -22,10 +24,10 @@ class Wizard(wx.Panel):
         # A Header for the wizard
         self.title = wx.Panel(self, style=wx.RAISED_BORDER)
         self.title.SetBackgroundColour((10, 47, 162))
-        self.title_txt = wx.StaticText(self.title, -1, u'Title') # Text displayed from objects "name" - object.GetName()
+        self.title_txt = wx.StaticText(self.title, label=GT(u'Title'))  # Text displayed from objects "name" - object.GetName()
         self.title_txt.SetForegroundColour((255, 255, 255))
         
-        headerfont = wx.Font(18, wx.DEFAULT, wx.NORMAL, wx.BOLD) # font to use in the header
+        headerfont = wx.Font(18, wx.DEFAULT, wx.NORMAL, wx.BOLD)  # font to use in the header
         self.title_txt.SetFont(headerfont)
         
         # Position the text in the header
@@ -35,11 +37,11 @@ class Wizard(wx.Panel):
         self.title.Layout()
         
         # Previous and Next buttons
-        self.button_prev = ButtonPrev(self, ID_PREV)
-        self.button_next = ButtonNext(self, ID_NEXT)
+        self.button_prev = ButtonPrev(self)
+        self.button_next = ButtonNext(self)
         
-        wx.EVT_BUTTON(self.button_prev, -1, self.ChangePage)
-        wx.EVT_BUTTON(self.button_next, -1, self.ChangePage)
+        wx.EVT_BUTTON(self.button_prev, wx.ID_ANY, self.ChangePage)
+        wx.EVT_BUTTON(self.button_next, wx.ID_ANY, self.ChangePage)
         
         self.ChangePageEvent, self.EVT_CHANGE_PAGE = NewCommandEvent()
         self.evt = self.ChangePageEvent(0)
@@ -63,61 +65,24 @@ class Wizard(wx.Panel):
         # These widgets are put into a list so that they are not automatically hidden
         self.permanent_children = (self.title, self.button_prev, self.button_next)
     
-    def SetPages(self, pages):
-        # Make sure all pages are hidden
-        children = self.GetChildren()
-        for child in children:
-            if child not in self.permanent_children:
-                child.Hide()
-        
-        self.ClearPages() # Remove any current pages from the wizard
-        
-        # These two item are just for checking to make sure that pages is right type
-        list = []
-        tuple = ()
-        if type(pages) not in (type(list), type(tuple)):
-            raise TypeError(u'Argument 2 of Wizard.SetPages() must be List or Tuple')
-        
-        for page in pages:
-            self.pages.append(page)
-            self.sizer.Insert(1, page, 1, wx.EXPAND)
-            # Show the first page
-            if page != pages[0]:
-                page.Hide()
-            else:
-                page.Show()
-                self.title_txt.SetLabel(page.GetName())
-        self.Layout()
-    
-    def ShowPage(self, id):
-        for p in self.pages:
-            if p.GetId() != id:
-                p.Hide()
-            else:
-                p.Show()
-                self.title_txt.SetLabel(p.GetName())
-                
-        self.Layout()
-        for child in self.GetChildren():
-            wx.PostEvent(child, self.evt)
-    
     
     ## TODO: Doxygen
-    def ChangePage(self, event):
+    def ChangePage(self, event=None):
         main_window = wx.GetApp().GetTopWindow()
         
-        id = event.GetEventObject().GetId()
+        ID = event.GetEventObject().GetId()
         
         # Get index of currently shown page
         for page in self.pages:
             if page.IsShown():
                 index = self.pages.index(page)
         
-        if id == ID_PREV:
+        if ID == ID_PREV:
             if index != 0:
                 index -= 1
-        elif id == ID_NEXT:
-            if index != len(self.pages)-1:
+        
+        elif ID == ID_NEXT:
+            if index != len(self.pages) - 1:
                 index += 1
         
         # Show the indexed page
@@ -127,54 +92,111 @@ class Wizard(wx.Panel):
         for p in main_window.pages:
             if self.pages[index].GetId() == p.GetId():
                 p.Check()
-#        for page in self.pages:
-#            if page != self.pages[index]:
-#                page.Hide()
-#            else:
-#                page.Show()
-#                self.title_txt.SetLabel(page.GetName())
-#        
-#        self.Layout()
-#        for child in self.GetChildren():
-#            wx.PostEvent(child, self.evt)
     
+    
+    ## TODO: Doxygen
     def ClearPages(self):
         for page in self.pages:
             self.sizer.Remove(page)
+        
         self.pages = []
-        # Re-enable the buttons if the have been disabled
+        
+        # Re-enable the buttons if they have been disabled
         self.EnableNext()
         self.EnablePrev()
     
-    def SetTitle(self, title):
-        self.title_txt.SetLabel(title)
-        self.Layout()
     
-    def EnableNext(self, value=True):
-        if type(value) in (type(True), type(1)):
-            if value:
-                self.button_next.Enable()
-            else:
-                self.button_next.Disable()
-        else:
-            raise TypeError(u'Must be bool or int value')
-    
+    ## TODO: Doxygen
     def DisableNext(self):
         self.EnableNext(False)
     
-    def EnablePrev(self, value=True):
-        if type(value) in (type(True), type(1)):
-            if value:
-                self.button_prev.Enable()
-            else:
-                self.button_prev.Disable()
-        else:
-            raise TypeError(u'Must be bool or int value')
     
+    ## TODO: Doxygen
     def DisablePrev(self):
         self.EnablePrev(False)
     
+    
+    ## TODO: Doxygen
+    def EnableNext(self, value=True):
+        if isinstance(value, (bool, int)):
+            if value:
+                self.button_next.Enable()
+            
+            else:
+                self.button_next.Disable()
+        
+        else:
+            # FIXME: Should not raise error here???
+            raise TypeError(u'Must be bool or int value')
+    
+    
+    ## TODO: Doxygen
+    def EnablePrev(self, value=True):
+        if isinstance(value, (bool, int)):
+            if value:
+                self.button_prev.Enable()
+            
+            else:
+                self.button_prev.Disable()
+        
+        else:
+            # FIXME: Should not raise error here???
+            raise TypeError(u'Must be bool or int value')
+    
+    
+    ## TODO: Doxygen
     def GetCurrentPage(self):
         for page in self.pages:
             if page.IsShown():
                 return page
+    
+    
+    ## TODO: Doxygen
+    def SetPages(self, pages):
+        # Make sure all pages are hidden
+        children = self.GetChildren()
+        for child in children:
+            if child not in self.permanent_children:
+                child.Hide()
+        
+        self.ClearPages() # Remove any current pages from the wizard
+        
+        if not isinstance(pages, (list, tuple)):
+            # FIXME: Should not raise error here???
+            raise TypeError(u'Argument 2 of Wizard.SetPages() must be List or Tuple')
+        
+        for page in pages:
+            self.pages.append(page)
+            self.sizer.Insert(1, page, 1, wx.EXPAND)
+            
+            # Show the first page
+            if page != pages[0]:
+                page.Hide()
+            
+            else:
+                page.Show()
+                self.title_txt.SetLabel(page.GetName())
+        
+        self.Layout()
+    
+    
+    ## TODO: Doxygen
+    def SetTitle(self, title):
+        self.title_txt.SetLabel(title)
+        self.Layout()
+    
+    
+    ## TODO: Doxygen
+    def ShowPage(self, ID):
+        for p in self.pages:
+            if p.GetId() != ID:
+                p.Hide()
+            
+            else:
+                p.Show()
+                self.title_txt.SetLabel(p.GetName())
+        
+        self.Layout()
+        
+        for child in self.GetChildren():
+            wx.PostEvent(child, self.evt)
