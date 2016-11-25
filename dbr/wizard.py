5608de616982483de183b9b53fe255e1e1d7218b
AntumDeluge <antumdeluge@gmail.com>
2016-11-24 18:17:33 -0800

    dbr.wizard: Merge some changes from unstable branch
    Set tooltips for previous & next buttons.
    Rename some variables, members, & methods.
    Set ID in Wizard's inherited class.

--- dbr/wizard.py
+++ dbr/wizard.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 
-# Wizard class for Debreate
+## \package dbr.wizard
 
 
 import wx
 from wx.lib.newevent import NewCommandEvent
 
-from dbr.buttons    import ButtonNext
-from dbr.buttons    import ButtonPrev
-from dbr.language   import GT
-from globals.ident  import ID_NEXT
-from globals.ident  import ID_PREV
+from dbr.buttons        import ButtonNext
+from dbr.buttons        import ButtonPrev
+from dbr.language       import GT
+from globals.ident      import ID_NEXT
+from globals.ident      import ID_PREV
+from globals.tooltips   import TT_wiz_next
+from globals.tooltips   import TT_wiz_prev
 
 
-## TODO: Doxygen
+## Wizard class for Debreate
 class Wizard(wx.Panel):
-    def __init__(self, parent, ID=wx.ID_ANY, page_list=None):
-        wx.Panel.__init__(self, parent, ID, page_list)
+    def __init__(self, parent, page_list=None):
+        wx.Panel.__init__(self, parent, wx.ID_ANY, page_list)
         
         # List of pages available in the wizard
         self.pages = []
@@ -24,10 +26,14 @@ class Wizard(wx.Panel):
         # A Header for the wizard
         self.title = wx.Panel(self, style=wx.RAISED_BORDER)
         self.title.SetBackgroundColour((10, 47, 162))
-        self.title_txt = wx.StaticText(self.title, label=GT(u'Title'))  # Text displayed from objects "name" - object.GetName()
+        
+        # Text displayed from objects "name" - object.GetName()
+        self.title_txt = wx.StaticText(self.title, label=GT(u'Title'))
         self.title_txt.SetForegroundColour((255, 255, 255))
         
-        headerfont = wx.Font(18, wx.DEFAULT, wx.NORMAL, wx.BOLD)  # font to use in the header
+        # font to use in the header
+        headerfont = wx.Font(18, wx.DEFAULT, wx.NORMAL, wx.BOLD)
+        
         self.title_txt.SetFont(headerfont)
         
         # Position the text in the header
@@ -38,7 +44,9 @@ class Wizard(wx.Panel):
         
         # Previous and Next buttons
         self.button_prev = ButtonPrev(self)
+        self.button_prev.SetToolTip(TT_wiz_prev)
         self.button_next = ButtonNext(self)
+        self.button_next.SetToolTip(TT_wiz_next)
         
         wx.EVT_BUTTON(self.button_prev, wx.ID_ANY, self.ChangePage)
         wx.EVT_BUTTON(self.button_next, wx.ID_ANY, self.ChangePage)
@@ -47,19 +55,19 @@ class Wizard(wx.Panel):
         self.evt = self.ChangePageEvent(0)
         
         # Button sizer includes header
-        button_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        button_sizer.AddSpacer(5)
-        button_sizer.Add(self.title, 1, wx.EXPAND)
-        button_sizer.Add(self.button_prev)
-        button_sizer.AddSpacer(5)
-        button_sizer.Add(self.button_next)
-        button_sizer.AddSpacer(5)
+        layout_buttons = wx.BoxSizer(wx.HORIZONTAL)
+        layout_buttons.AddSpacer(5)
+        layout_buttons.Add(self.title, 1, wx.EXPAND)
+        layout_buttons.Add(self.button_prev)
+        layout_buttons.AddSpacer(5)
+        layout_buttons.Add(self.button_next)
+        layout_buttons.AddSpacer(5)
         
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer.Add(button_sizer, 0, wx.EXPAND)
+        self.layout_main = wx.BoxSizer(wx.VERTICAL)
+        self.layout_main.Add(layout_buttons, 0, wx.EXPAND)
         
+        self.SetSizer(self.layout_main)
         self.SetAutoLayout(True)
-        self.SetSizer(self.sizer)
         self.Layout()
         
         # These widgets are put into a list so that they are not automatically hidden
@@ -68,20 +76,18 @@ class Wizard(wx.Panel):
     
     ## TODO: Doxygen
     def ChangePage(self, event=None):
-        main_window = wx.GetApp().GetTopWindow()
-        
-        ID = event.GetEventObject().GetId()
+        event_id = event.GetEventObject().GetId()
         
         # Get index of currently shown page
         for page in self.pages:
             if page.IsShown():
                 index = self.pages.index(page)
         
-        if ID == ID_PREV:
+        if event_id == ID_PREV:
             if index != 0:
                 index -= 1
         
-        elif ID == ID_NEXT:
+        elif event_id == ID_NEXT:
             if index != len(self.pages) - 1:
                 index += 1
         
@@ -89,7 +95,7 @@ class Wizard(wx.Panel):
         self.ShowPage(self.pages[index].GetId())
         
         # Check current page in "pages menu"
-        for p in main_window.pages:
+        for p in wx.GetApp().GetTopWindow().pages:
             if self.pages[index].GetId() == p.GetId():
                 p.Check()
     
@@ -97,7 +103,7 @@ class Wizard(wx.Panel):
     ## TODO: Doxygen
     def ClearPages(self):
         for page in self.pages:
-            self.sizer.Remove(page)
+            self.layout_main.Remove(page)
         
         self.pages = []
         
@@ -145,10 +151,10 @@ class Wizard(wx.Panel):
     
     
     ## TODO: Doxygen
-    def GetCurrentPage(self):
+    def GetCurrentPageId(self):
         for page in self.pages:
             if page.IsShown():
-                return page
+                return page.GetId()
     
     
     ## TODO: Doxygen
@@ -159,7 +165,8 @@ class Wizard(wx.Panel):
             if child not in self.permanent_children:
                 child.Hide()
         
-        self.ClearPages() # Remove any current pages from the wizard
+        # Remove any current pages from the wizard
+        self.ClearPages()
         
         if not isinstance(pages, (list, tuple)):
             # FIXME: Should not raise error here???
@@ -167,7 +174,7 @@ class Wizard(wx.Panel):
         
         for page in pages:
             self.pages.append(page)
-            self.sizer.Insert(1, page, 1, wx.EXPAND)
+            self.layout_main.Insert(1, page, 1, wx.EXPAND)
             
             # Show the first page
             if page != pages[0]:
@@ -187,9 +194,9 @@ class Wizard(wx.Panel):
     
     
     ## TODO: Doxygen
-    def ShowPage(self, ID):
+    def ShowPage(self, page_id):
         for p in self.pages:
-            if p.GetId() != ID:
+            if p.GetId() != page_id:
                 p.Hide()
             
             else:
