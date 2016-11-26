3f721bbcf190a18a19ea9b4e9dc0a1db6a055745
AntumDeluge <antumdeluge@gmail.com>
2016-11-25 22:01:52 -0800

    globals.tooltips: Changes to Control page tooltips
    Rename 'arch' identifier to 'architecture'.
    Change description of 'architecture' to pad '=' with spaces.

--- globals/tooltips.py
+++ globals/tooltips.py
@@ -37,9 +37,9 @@ TT_control = {
     u'version': GT(u'Package/Software release version'),
     u'maintainer': GT(u'Package/Software maintainer\'s full name'),
     u'email': GT(u'Package/Software maintainer\'s email address'),
-    u'arch': (
+    u'architecture': (
         GT(u'Platform on which package/software is meant to run'), u'',
-        GT(u'all=platform independent'),
+        GT(u'all = platform independent'),
         ),
     u'section': GT(u'Section under which package managers will list this package'),
     u'priority': GT(u'Urgency of this package update'),
