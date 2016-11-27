85774127f44ad0b0509a7399a6ebbc1c9912927f
AntumDeluge <antumdeluge@gmail.com>
2016-11-26 23:02:14 -0800

    dbr.charctrl: Simplify OnKeyUp method & remove unused code

--- dbr/charctrl.py
+++ dbr/charctrl.py
@@ -10,6 +10,13 @@
 
 import wx
 
+from dbr.functions import TextIsEmpty
+
+
+# List of characters that cannot be entered
+# NOTE: 46 = ".", 47 = "/"
+invalid_chars = (u'/', u'\\', u'_')
+
 
 ## A customized text area that disallows certain character input
 #  
@@ -18,48 +25,25 @@ class CharCtrl(wx.TextCtrl):
     def __init__(self, parent, ctrl_id=wx.ID_ANY, value=wx.EmptyString, name=wx.TextCtrlNameStr):
         wx.TextCtrl.__init__(self, parent, ctrl_id, value, name=name)
         
-        ## List of characters that cannot be entered
-        #  
-        #  NOTE: 46 = ".", 47 = "/"
-        self.invalid_chars = (u' ', u'/', u'_')
-        
-        ## List of keys that should not be affected when using the spacebar
-        #  
-        #  ??? FIXME: 'spacebar' or 'shift' typo?
-        self.shift_exceptions = (wx.WXK_LEFT, wx.WXK_RIGHT, wx.WXK_UP, wx.WXK_DOWN)
-        
-        ## List of keys that should not be affected when using the Ctrl key
-        self.ctrl_exceptions = (u'A', u'A')
-        
         wx.EVT_KEY_UP(self, self.OnKeyUp)
     
     
     ## Actions to take when key is released
     def OnKeyUp(self, event=None):
-        modifier = event.GetModifiers()
-        keycode = event.GetKeyCode()
-        char = self.GetValue()[self.GetInsertionPoint()-1]
+        insertion_point = self.GetInsertionPoint()
+        text = self.GetValue()
+        original_text = text
         
-        def ReplaceChar():
-            value = self.GetValue()
-            insertion = self.GetInsertionPoint()
-            total_chars = len(value)
-            while total_chars > 0:
-                total_chars -= 1
-                if value[total_chars] in self.invalid_chars:
-                    self.Replace(total_chars, total_chars+1, u'-')
+        # Remove whitespace
+        for C in (u' ', u'\t'):
+            if C in text:
+                text = text.replace(C, u'-')
+        
+        if not TextIsEmpty(text):
+            for C in invalid_chars:
+                if C in text:
+                    text = text.replace(C, u'-')
             
-            self.SetInsertionPoint(insertion)
-        
-        
-        if modifier == wx.MOD_SHIFT and char in self.invalid_chars:
-            ReplaceChar()
-        
-        elif char in self.invalid_chars:
-            ReplaceChar()
-        
-        else:
-            pass
-        
-        if event:
-            event.Skip()
+            if text != original_text:
+                self.SetValue(text)
+                self.SetInsertionPoint(insertion_point)
