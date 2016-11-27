a9a50ce6a662621199bd6abbacab0181380ad7f2
AntumDeluge <antumdeluge@gmail.com>
2016-11-26 20:26:55 -0800

    wiz_bin.copyright: Code cleanup
    Add licensing header.
    Rename some variables & member variables.
    Sort layout & event handlers at end of constructor.
    Use 'Bind' method for button event handlers.

--- wiz_bin/copyright.py
+++ wiz_bin/copyright.py
@@ -2,6 +2,9 @@
 
 ## \package wiz_bin.copyright
 
+# MIT licensing
+# See: docs/LICENSE.txt
+
 
 import wx
 
@@ -18,45 +21,50 @@ class Panel(wx.ScrolledWindow):
         
         self.SetScrollbars(0, 20, 0, 0)
         
-        lic_options = (
+        opts_licenses = (
             u'Apache-2.0', u'Artistic', u'BSD', u'GFDL', u'GFDL-1.2',
             u'GFDL-1.3', u'GPL', u'GPL-1', u'GPL-2', u'GPL-3', u'LGPL',
             u'LGPL-2', u'LGPL-2.1', u'LGPL-3'
         )
         
-        template_btn = wx.Button(self, label=GT(u'Generate Template'), name=u'full')
+        btn_template = wx.Button(self, label=GT(u'Generate Template'), name=u'full')
         
-        self.template_lic = wx.Choice(self, choices=lic_options, name=u'list')
-        self.template_lic.SetSelection(0)
-        
-        wx.EVT_BUTTON(template_btn, -1, self.GenerateTemplate)
+        self.sel_templates = wx.Choice(self, choices=opts_licenses, name=u'list')
+        self.sel_templates.SetSelection(0)
         
         template_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        template_sizer.Add(template_btn, 1)
-        template_sizer.Add(self.template_lic, 1)
+        template_sizer.Add(btn_template, 1)
+        template_sizer.Add(self.sel_templates, 1)
         
-        self.cp_display = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        
-        main_sizer = wx.BoxSizer(wx.VERTICAL)
-        main_sizer.Add(template_sizer, 0, wx.ALL, 5)
-        main_sizer.Add(self.cp_display, 1, wx.EXPAND|wx.ALL, 5)
-        
-        self.SetAutoLayout(True)
-        self.SetSizer(main_sizer)
-        self.Layout()
-        
+        self.dsp_copyright = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         
         SetPageToolTips(self)
+        
+        # *** Layout *** #
+        
+        lyt_main = wx.BoxSizer(wx.VERTICAL)
+        lyt_main.Add(template_sizer, 0, wx.ALL, 5)
+        lyt_main.Add(self.dsp_copyright, 1, wx.EXPAND|wx.ALL, 5)
+        
+        self.SetAutoLayout(True)
+        self.SetSizer(lyt_main)
+        self.Layout()
+        
+        # *** Event handlers *** #
+        
+        btn_template.Bind(wx.EVT_BUTTON, self.GenerateTemplate)
     
     
     ## TODO: Doxygen
     def DestroyLicenseText(self):
         main_window = wx.GetApp().GetTopWindow()
         
-        empty = TextIsEmpty(self.cp_display.GetValue())
+        empty = TextIsEmpty(self.dsp_copyright.GetValue())
         
         if not empty:
-            if wx.MessageDialog(main_window, GT(u'This will destroy all license text. Do you want to continue?'), GT(u'Warning'),
+            if wx.MessageDialog(main_window,
+                    GT(u'This will destroy all license text. Do you want to continue?'),
+                    GT(u'Warning'),
                     wx.YES_NO|wx.NO_DEFAULT|wx.ICON_EXCLAMATION).ShowModal() == wx.ID_NO:
                 return 0
         
@@ -74,24 +82,24 @@ class Panel(wx.ScrolledWindow):
         if not self.DestroyLicenseText():
             return
         
-        self.cp_display.Clear()
+        self.dsp_copyright.Clear()
         
-        lic_path = u'/usr/share/common-licenses/{}'.format(self.template_lic.GetString(self.template_lic.GetSelection()))
+        lic_path = u'/usr/share/common-licenses/{}'.format(self.sel_templates.GetStringSelection())
         cpright = u'Copyright: <year> <copyright holder> <email>'
         
-        self.cp_display.SetValue(u'{}\n\n{}'.format(cpright, lic_path))
+        self.dsp_copyright.SetValue(u'{}\n\n{}'.format(cpright, lic_path))
     
     
     ## TODO: Doxygen
     def GetCopyright(self):
-        return self.cp_display.GetValue()
+        return self.dsp_copyright.GetValue()
     
     
     ## TODO: Doxygen
     def ResetAllFields(self):
-        self.cp_display.Clear()
+        self.dsp_copyright.Clear()
     
     
     ## TODO: Doxygen
     def SetCopyright(self, data):
-        self.cp_display.SetValue(data)
+        self.dsp_copyright.SetValue(data)
