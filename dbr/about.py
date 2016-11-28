819f13088b5db3dade9a4d2e2dd7cdd88195aff1
AntumDeluge <antumdeluge@gmail.com>
2016-11-27 21:26:58 -0800

    dbr.about: Use GetContainerItemCount function for sizers

--- dbr/about.py
+++ dbr/about.py
@@ -11,7 +11,8 @@ import commands, os, shutil, wx
 from dbr.buttons            import ButtonConfirm
 from dbr.custom             import Hyperlink
 from dbr.font               import MONOSPACED_MD
-from dbr.functions          import CreateTempDirectory
+from dbr.functions          import CreateTempDirectory, GetContainerItemCount
+from dbr.functions          import GetContainerItemCount
 from dbr.functions          import GetFileMimeType
 from dbr.functions          import GetYear
 from dbr.functions          import RemoveTempDirectory
@@ -154,7 +155,7 @@ class AboutDialog(wx.Dialog):
     #  \param graphic
     #        Path to image file
     def SetGraphic(self, graphic):
-        insertion_point = self.about_layout_V1.GetItemCount() - 1
+        insertion_point = GetContainerItemCount(self.about_layout_V1) - 1
         
         image = wx.Image(graphic)
         image.Rescale(64, 64, wx.IMAGE_QUALITY_HIGH)
@@ -175,7 +176,7 @@ class AboutDialog(wx.Dialog):
     #  \param version
     #        String to display
     def SetVersion(self, version):
-        insertion_point = self.about_layout_V1.GetItemCount() - 1
+        insertion_point = GetContainerItemCount(self.about_layout_V1) - 1
         
         app_label = wx.StaticText(
             self.t_about,
@@ -199,7 +200,7 @@ class AboutDialog(wx.Dialog):
     #  \param author
     #        String to display
     def SetAuthor(self, author):
-        insertion_point = self.about_layout_V1.GetItemCount() - 1
+        insertion_point = GetContainerItemCount(self.about_layout_V1) - 1
         
         self.about_layout_V1.Insert(
             insertion_point,
@@ -227,7 +228,7 @@ class AboutDialog(wx.Dialog):
     #  \param url_list
     #        \b \e tuple : Website labels & URL definitions
     def SetWebsites(self, url_list):
-        insertion_point = self.about_layout_V1.GetItemCount() - 1
+        insertion_point = GetContainerItemCount(self.about_layout_V1) - 1
         
         link_layout = wx.BoxSizer(wx.VERTICAL)
         for label, link in url_list:
@@ -251,7 +252,7 @@ class AboutDialog(wx.Dialog):
     ## Displays a description about the app on the 'about' tab
     def SetDescription(self, desc):
         # Place between spacers
-        insertion_point = self.about_layout_V1.GetItemCount() - 1
+        insertion_point = GetContainerItemCount(self.about_layout_V1) - 1
         
         self.about_layout_V1.Insert(
             insertion_point,
