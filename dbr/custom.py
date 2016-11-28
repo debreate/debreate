f026af8c8ca223a0ab7686e2774b7c1efaccb0ef
AntumDeluge <antumdeluge@gmail.com>
2016-11-28 03:45:13 -0800

    dbr.custom: Create custom ComboBox class
    Derived from OwnerDrawnComboBox. This class is a workaround for wx
    version older than 3.0. It toggles the background color of the text area
    when disabled/enabled.

--- dbr/custom.py
+++ dbr/custom.py
@@ -7,6 +7,7 @@
 
 
 import os, sys, webbrowser, wx
+from wx.combo import OwnerDrawnComboBox
 from wx.lib.docview import PathOnly
 
 from dbr.language   import GT
@@ -19,6 +20,42 @@ db_here = PathOnly(__file__).decode(u'utf-8')
 # FIXME: This should be import from dbr.functions
 def TextIsEmpty(text):
     return text.strip(u' \t\n') == wx.EmptyString
+
+
+## Custom combo box that sets background colors when enabled/disabled
+#  
+#  This is a workaround for wx versions older than 3.0
+class ComboBox(OwnerDrawnComboBox):
+    def __init__(self, parent, ID=wx.ID_ANY, value=wx.EmptyString, pos=wx.DefaultPosition,
+            size=wx.DefaultSize, choices=None, style=0,
+            validator=wx.DefaultValidator, name=wx.ComboBoxNameStr):
+        OwnerDrawnComboBox.__init__(self, parent, ID, value, pos, size, choices, style,
+                validator, name)
+        
+        if wx.MAJOR_VERSION < 3:
+            self.clr_disabled = self.GetBackgroundColour()
+            self.clr_enabled = self.GetTextCtrl().GetBackgroundColour()
+    
+    
+    ## TODO: Doxygen
+    def Disable(self):
+        return self.Enable(False)
+    
+    
+    ## TODO: Doxygen
+    def Enable(self, *args, **kwargs):
+        return_value =  OwnerDrawnComboBox.Enable(self, *args, **kwargs)
+        
+        if wx.MAJOR_VERSION < 3:
+            text_area = self.GetTextCtrl()
+            
+            if self.IsEnabled():
+                text_area.SetBackgroundColour(self.clr_enabled)
+            
+            else:
+                text_area.SetBackgroundColour(self.clr_disabled)
+        
+        return return_value
 
 
 ## Dialog shown when Debreate is run for first time
