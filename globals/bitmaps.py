9c79eac6352779f9bd56372b24e2e6ce5c66765e
AntumDeluge <antumdeluge@gmail.com>
2016-11-27 17:13:29 -0800

    globals.bitmaps: Add ICON_EXCLAMATION & ICON_QUESTION

--- globals/bitmaps.py
+++ globals/bitmaps.py
@@ -15,7 +15,9 @@ from globals.paths import PATH_app
 PATH_bitmaps = u'{}/bitmaps'.format(PATH_app)
 
 ICON_ERROR = ConcatPaths((PATH_bitmaps, u'error64.png'))
-ICON_INFORMATION = ConcatPaths((PATH_bitmaps, u'question64.png'))
+ICON_EXCLAMATION = ICON_ERROR
+ICON_QUESTION = ConcatPaths((PATH_bitmaps, u'question64.png'))
+ICON_INFORMATION = ICON_QUESTION
 
 BUTTON_HELP = wx.Bitmap(ConcatPaths((PATH_bitmaps, u'question32.png')))
 BUTTON_REFRESH = wx.Bitmap(ConcatPaths((PATH_bitmaps, u'btn-refresh32.png')))
