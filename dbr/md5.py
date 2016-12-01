8c4946f163c93386a26b8e26bb16f924827538b9
AntumDeluge <antumdeluge@gmail.com>
2016-12-01 12:34:42 -0800

    dbr.md5: Changes to MD5Hasher class
    Require constructor argument 'control' to retrieve label.
    Add some Doxygen documentation.
    If md5sum command fails to be found, show an error dialog in 'WriteMd5'
    method.
    Set optional argument 'parent' for error dialog in 'WriteMd5' method.

--- dbr/md5.py
+++ dbr/md5.py
@@ -5,15 +5,27 @@
 
 import commands, os
 
-from globals.commands import CMD_md5sum
+from dbr.dialogs            import ErrorDialog
+from dbr.language           import GT
+from dbr.log                import Logger
+from globals.commands       import CMD_md5sum
+from globals.wizardhelper   import GetTopWindow
+
+
+# FIXME: This module uses the following deprecated modules
+# - commands
 
 
 ## Object for creating MD5 hashes
+#  
+#  \param control
+#        \b \e wx.CheckBox : Translated label of this control is used for error dialog
 class MD5Hasher:
-    def __init__(self):
-        pass
+    def __init__(self, control):
+        self.md5_box_name = control.GetLabel()
     
     
+    ## TODO: Doxygen
     def IsExecutable(self, filename):
         # Find out if the file is an executable
         executable = os.access(filename, os.X_OK) #another python version
@@ -21,7 +33,30 @@ class MD5Hasher:
         return bool(executable)
     
     
-    def WriteMd5(self, builddir, tempdir):
+    ## TODO: Doxygen
+    def WriteMd5(self, builddir, tempdir, parent=None):
+        # Show an error if the 'md5sum' command does not exist
+        # This is only a failsafe & should never happen
+        if not CMD_md5sum:
+            if not parent:
+                parent = GetTopWindow()
+            
+            err_msg1 = GT(u'The "md5sum" command was not found on the system.')
+            err_msg2 = GT(u'Uncheck the "{}" box.').format(self.md5_box_name)
+            err_msg3 = GT(u'Please report this error to one of the following addresses:')
+            err_url1 = u'https://github.com/AntumDeluge/debreate/issues'
+            err_url2 = u'https://sourceforge.net/p/debreate/bugs/'
+            
+            Logger.Error(__name__,
+                    u'{} {} {}\n\t{}\n\t{}'.format(err_msg1, err_msg2, err_msg3, err_url1, err_url2))
+            
+            md5_error = ErrorDialog(parent, u'{}\n{}\n\n{}'.format(err_msg1, err_msg2, err_msg3))
+            md5_error.AddURL(err_url1)
+            md5_error.AddURL(err_url2)
+            md5_error.ShowModal()
+            
+            return False
+        
         tempdir = tempdir.encode(u'utf-8')
         os.chdir(builddir)
         
@@ -41,6 +76,9 @@ class MD5Hasher:
             sum_join = u''.join(sum_split)
             md5_list.append(sum_join)
         
-        FILE_BUFFER = open(u'{}/DEBIAN/md5sums'.format(tempdir), u'w')  # Create the md5sums file in the "DEBIAN" directory
-        FILE_BUFFER.write(u'{}\n'.format(u'\n'.join(md5_list)))  # Write the final list to the file
-        FILE_BUFFER.close() # Save the file
+        # Create the md5sums file in the "DEBIAN" directory
+        FILE_BUFFER = open(u'{}/DEBIAN/md5sums'.format(tempdir), u'w')
+        FILE_BUFFER.write(u'{}\n'.format(u'\n'.join(md5_list)))
+        FILE_BUFFER.close()
+        
+        return True
