bfa2d1330d532d253ded3f56a78a655f09298fe0
AntumDeluge <antumdeluge@gmail.com>
2016-12-04 13:37:10 -0800

    Add globals.colors modules for setting custom color constants

---
+++ globals/colors.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+## \package globals.colors
+
+# MIT licensing
+# See: docs/LICENSE.txt
+
+
+from wx import Colour as Color
+
+
+# Yellow
+COLOR_tooltip = Color(255, 255, 0)
+# Orange
+COLOR_warn = Color(255, 165, 0)
