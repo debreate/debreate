4f1a8124f9cce3943bfcf50e8581bda59bdcc662
AntumDeluge <antumdeluge@gmail.com>
2016-12-04 19:48:08 -0800

    globals.bitmaps: Add 'LOGO' for main window icon

--- globals/bitmaps.py
+++ globals/bitmaps.py
@@ -14,6 +14,8 @@ from globals.paths import PATH_app
 
 PATH_bitmaps = u'{}/bitmaps'.format(PATH_app)
 
+LOGO = ConcatPaths((PATH_bitmaps, u'debreate64.png'))
+
 ICON_ERROR = ConcatPaths((PATH_bitmaps, u'error64.png'))
 ICON_EXCLAMATION = ICON_ERROR
 ICON_QUESTION = ConcatPaths((PATH_bitmaps, u'question64.png'))
