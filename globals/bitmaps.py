b8eeaa4398e9ed12dfe28fba882ccadbb21ed04f
AntumDeluge <antumdeluge@gmail.com>
2016-12-09 03:24:15 -0800

    globals.bitmaps: Add new bitmaps:
    Change ICON_INFORMATION bitmap to 'information64.png'.
    Add ICON_WARNING with bitmap 'warning64.png'.

--- globals/bitmaps.py
+++ globals/bitmaps.py
@@ -19,7 +19,8 @@ LOGO = ConcatPaths((PATH_bitmaps, u'debreate64.png'))
 ICON_ERROR = ConcatPaths((PATH_bitmaps, u'error64.png'))
 ICON_EXCLAMATION = ICON_ERROR
 ICON_QUESTION = ConcatPaths((PATH_bitmaps, u'question64.png'))
-ICON_INFORMATION = ICON_QUESTION
+ICON_INFORMATION = ConcatPaths((PATH_bitmaps, u'information64.png'))
+ICON_WARNING = ConcatPaths((PATH_bitmaps, u'warning64.png'))
 
 ICON_CLOCK = wx.Bitmap(ConcatPaths((PATH_bitmaps, u'clock16.png')))
 ICON_GLOBE = wx.Bitmap(ConcatPaths((PATH_bitmaps, u'globe16.png')))
