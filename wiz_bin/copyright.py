d59b2ee23729152f8c4ee6515ae0211360b29d54
AntumDeluge <antumdeluge@gmail.com>
2016-12-10 20:29:56 -0800

    wiz_bin.copyright: Merge some changes from unstable branch
    Template list & buttons for full & simple templates.

--- wiz_bin/copyright.py
+++ wiz_bin/copyright.py
@@ -6,16 +6,24 @@
 # See: docs/LICENSE.txt
 
 
-import wx
+import os, wx
 
 from dbr.dialogs            import ConfirmationDialog
 from dbr.functions          import GetSystemLicensesList
 from dbr.functions          import TextIsEmpty
 from dbr.language           import GT
+from dbr.templates          import GetLicenseTemplatesList
+from dbr.templates          import application_licenses_path
+from dbr.templates          import local_licenses_path
 from dbr.textinput          import MonospaceTextCtrl
 from globals                import ident
+from globals.constants      import system_licenses_path
 from globals.tooltips       import SetPageToolTips
 from globals.wizardhelper   import GetTopWindow
+
+
+# Globals
+copyright_header = GT(u'Copyright © {} <copyright holder(s)> [<email>]\n\n')
 
 
 ## Copyright page
@@ -28,31 +36,60 @@ class Panel(wx.ScrolledWindow):
         # FIXME: Ignore symbolic links
         opts_licenses = GetSystemLicensesList()
         
+        # FIXME: Change variable name to 'self.builtin_licenses
+        self.opts_local_licenses = GetLicenseTemplatesList()
+        
+        # Do not use local licenses if already located on system
+        for lic in opts_licenses:
+            if lic in self.opts_local_licenses:
+                self.opts_local_licenses.remove(lic)
+        
+        # Add the remaining licenses to the selection list
+        for lic in self.opts_local_licenses:
+            opts_licenses.append(lic)
+        
+        opts_licenses.sort(key=unicode.lower)
+        
         ## A list of available license templates
         self.sel_templates = wx.Choice(self, choices=opts_licenses, name=u'list»')
         self.sel_templates.default = 0
         self.sel_templates.SetSelection(self.sel_templates.default)
         
-        btn_template = wx.Button(self, label=GT(u'Generate Template'), name=u'gen»')
+        btn_template = wx.Button(self, label=GT(u'Generate Template'), name=u'full»')
+        self.btn_template_simple = wx.Button(self, label=GT(u'Generate Linked Template'), name=u'link»')
         
         if not self.sel_templates.GetCount():
-            btn_template.Enable(False)
             self.sel_templates.Enable(False)
+            btn_template.Enable(False)
+            self.btn_template_simple.Enable(False)
         
         ## Area where license text is displayed
         self.dsp_copyright = MonospaceTextCtrl(self, name=u'license')
         
         SetPageToolTips(self)
         
+        if self.sel_templates.IsEnabled():
+            self.OnSelectTemplate(self.sel_templates)
+        
         # *** Layout *** #
         
         lyt_buttons = wx.BoxSizer(wx.HORIZONTAL)
         lyt_buttons.Add(btn_template, 1)
-        lyt_buttons.Add(self.sel_templates, 1)
+        lyt_buttons.Add(self.btn_template_simple, 1)
+        
+        lyt_label = wx.BoxSizer(wx.HORIZONTAL)
+        lyt_label.Add(
+            wx.StaticText(self, label=GT(u'Available Templates')),
+            0,
+            wx.TOP|wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER,
+            5
+        )
+        lyt_label.Add(self.sel_templates, 0, wx.TOP, 5)
+        lyt_label.Add(lyt_buttons, 1, wx.LEFT, 150)
         
         lyt_main = wx.BoxSizer(wx.VERTICAL)
         lyt_main.AddSpacer(10)
-        lyt_main.Add(lyt_buttons, 0, wx.LEFT|wx.RIGHT|wx.BOTTOM, 5)
+        lyt_main.Add(lyt_label, 0, wx.LEFT|wx.RIGHT|wx.BOTTOM, 5)
         lyt_main.Add(self.dsp_copyright, 1, wx.EXPAND|wx.LEFT|wx.RIGHT|wx.BOTTOM, 5)
         
         self.SetAutoLayout(True)
@@ -61,7 +98,10 @@ class Panel(wx.ScrolledWindow):
         
         # *** Event handlers *** #
         
+        self.sel_templates.Bind(wx.EVT_CHOICE, self.OnSelectTemplate)
+        
         btn_template.Bind(wx.EVT_BUTTON, self.GenerateTemplate)
+        self.btn_template_simple.Bind(wx.EVT_BUTTON, self.GenerateTemplate)
     
     
     ## TODO: Doxygen
@@ -106,8 +146,45 @@ class Panel(wx.ScrolledWindow):
     
     
     ## TODO: Doxygen
+    def GetLicensePath(self, template_name):
+        # User templates have priority
+        license_path = u'{}/{}'.format(local_licenses_path, template_name)
+        if os.path.isfile(license_path):
+            return license_path
+        
+        license_path = u'{}/{}'.format(system_licenses_path, template_name)
+        if os.path.isfile(license_path):
+            return license_path
+        
+        license_path = u'{}/{}'.format(application_licenses_path, template_name)
+        if os.path.isfile(license_path):
+            return license_path
+        
+        return None
+    
+    
+    ## TODO: Doxygen
     def IsBuildExportable(self):
         return not TextIsEmpty(self.dsp_copyright.GetValue())
+    
+    
+    ## TODO: Doxygen
+    def OnSelectTemplate(self, event=None):
+        if isinstance(event, wx.Choice):
+            choice = event
+        
+        else:
+            choice = event.GetEventObject()
+        
+        template = choice.GetString(choice.GetSelection())
+        
+        if template in self.opts_local_licenses:
+            self.btn_template_simple.Disable()
+        
+        else:
+            self.btn_template_simple.Enable()
+        
+        self.SetTemplateToolTip()
     
     
     ## TODO: Doxygen
@@ -118,3 +195,15 @@ class Panel(wx.ScrolledWindow):
     ## TODO: Doxygen
     def SetCopyright(self, data):
         self.dsp_copyright.SetValue(data)
+    
+    
+    ## TODO: Doxygen
+    def SetTemplateToolTip(self):
+        license_name = self.sel_templates.GetString(self.sel_templates.GetSelection())
+        license_path = self.GetLicensePath(license_name)
+        
+        if license_path:
+            self.sel_templates.SetToolTip(wx.ToolTip(license_path))
+            return
+        
+        self.sel_templates.SetToolTip(None)
