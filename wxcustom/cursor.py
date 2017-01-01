a7441aa285f89ac62272886cea662afd5c3bea4f
AntumDeluge <antumdeluge@gmail.com>
2017-01-01 13:40:41 -0800

    wxcustom.cursor: Change how GetCursor works:
    - Use Path_bitmaps instead of using sub-directory 'cursor'.
    - Return wx.NullCursor if image not found.
    - Set image type from arguments.

--- wxcustom/cursor.py
+++ wxcustom/cursor.py
@@ -8,21 +8,24 @@
 
 import os, wx
 
-from globals.paths import ConcatPaths
-from globals.paths import PATH_app
+from globals.bitmaps    import PATH_bitmaps
+from globals.paths      import ConcatPaths
 
 
-PATH_cursors = u'{}/bitmaps/cursor'.format(PATH_app)
-
-def GetCursor(name, size=16):
-    global PATH_cursors
-    
-    cursor_bitmap = ConcatPaths((PATH_cursors, str(size), u'{}.png'.format(name)))
+## Retrieves an image from bitmaps dir & creates a wx.Cursor
+#  
+#  \param name
+#    \b \e string : Base filename of the image
+#  \param size
+#    \b \e int : Image size (denotes subfolder to search)
+#  \param img_type
+#    \b \e string : Image type / filename suffix
+#  \return
+#    \b \e wx.Cursor : Either a new cursor using the retrieved image, or wx.NullCursor
+def GetCursor(name, size=16, img_type=u'png'):
+    cursor_bitmap = ConcatPaths((PATH_bitmaps, str(size), u'{}.{}'.format(name, img_type)))
     
     if not os.path.isfile(cursor_bitmap):
-        return None
+        return wx.NullCursor
     
     return wx.Cursor(cursor_bitmap, wx.BITMAP_TYPE_PNG)
-
-
-CUR_FOLDER = GetCursor(u'folder')
