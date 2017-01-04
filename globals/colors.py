d08f57dfe794281ec304ac1114f7cf14830dde8a
AntumDeluge <antumdeluge@gmail.com>
2017-01-04 12:36:59 -0800

    globals.colors: Add 'COLOR_error' & 'COLOR_executable'

--- globals/colors.py
+++ globals/colors.py
@@ -13,3 +13,7 @@ from wx import Colour as Color
 COLOR_tooltip = Color(255, 255, 0)
 # Red-orange
 COLOR_warn = Color(255, 143, 115)
+# Red
+COLOR_error = Color(255, 0, 0)
+
+COLOR_executable = COLOR_error
