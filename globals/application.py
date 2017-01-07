ce63a18734c8537f47466e545186ca823ee83bb1
AntumDeluge <antumdeluge@gmail.com>
2017-01-06 16:35:48 -0800

    Increment development version to 2

--- globals/application.py
+++ globals/application.py
@@ -42,7 +42,7 @@ VERSION_tuple = (VERSION_maj, VERSION_min, VERSION_rel)
 VERSION_string = u'{}.{}.{}'.format(VERSION_maj, VERSION_min, VERSION_rel)
 
 # Development version: Increment for every development release
-VERSION_dev = 1
+VERSION_dev = 2
 if VERSION_dev:
     VERSION_string = u'{}-dev{}'.format(VERSION_string, VERSION_dev)
 
