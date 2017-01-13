869cf8dcba14e7883552f0bad5b0b26e0ca2b26a
AntumDeluge <antumdeluge@gmail.com>
2017-01-13 04:28:18 -0800

    Merge dbr.statusbar from unstable branch

---
+++ dbr/statusbar.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+## \package dbr.statusbar
+
+# MIT licensing
+# See: docs/LICENSE.txt
+
+
+import wx
+
+
+## A status bar for compatibility between wx 3.0 & older versions
+class StatusBar(wx.StatusBar):
+    if wx.MAJOR_VERSION > 2:
+        sb_style = wx.STB_DEFAULT_STYLE
+    
+    else:
+        sb_style = wx.ST_SIZEGRIP
+    
+    def __init__(self, parent, ID=wx.ID_ANY, style=sb_style,
+                name=wx.StatusLineNameStr):
+        wx.StatusBar.__init__(self, parent, ID, style, name)
+        
+        parent.SetStatusBar(self)
