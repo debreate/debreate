1f1006a7c8b0b824b656a42d1ceea8ec145855ce
AntumDeluge <antumdeluge@gmail.com>
2017-01-13 01:04:15 -0800

    dbr.firstrun: Merge LaunchFirstRun function from 0.7 branch

--- dbr/firstrun.py
+++ dbr/firstrun.py
@@ -6,10 +6,40 @@
 # See: docs/LICENSE.txt
 
 
-import wx
+import os, wx
 
+from dbr.config     import ConfCode
+from dbr.config     import InitializeConfig
+from dbr.config     import default_config
+from dbr.dialogs    import ShowErrorDialog
 from dbr.language   import GT
+from dbr.log        import Logger
 from globals.paths  import PATH_app
+
+
+## Shows the first run dialog
+def LaunchFirstRun(debreate_app):
+    FR_dialog = FirstRun()
+    debreate_app.SetTopWindow(FR_dialog)
+    FR_dialog.ShowModal()
+    
+    init_conf_code = InitializeConfig()
+    
+    Logger.Debug(__name__, u'Configuration initialized: {}'.format(init_conf_code == ConfCode.SUCCESS))
+    
+    if (init_conf_code != ConfCode.SUCCESS) or (not os.path.isfile(default_config)):
+        msg_l1 = GT(u'An error occurred trying to create the configuration file:')
+        msg_l2 = GT(u'Please report this error to Debreate\'s developers')
+        ShowErrorDialog(u'{} {}\n\n{}'.format(msg_l1, default_config, msg_l2))
+        
+        return init_conf_code
+    
+    FR_dialog.Destroy()
+    
+    # Delete first run dialog from memory
+    del(FR_dialog)
+    
+    return init_conf_code
 
 
 ## Dialog shown when Debreate is run for first time
