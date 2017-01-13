f16762d3bca72535a5a0449a324f250761863be9
AntumDeluge <antumdeluge@gmail.com>
2017-01-13 01:01:37 -0800

    Move FirstRun class to dbr.firstrun

--- init.py
+++ init.py
@@ -112,8 +112,8 @@ from dbr.config             import default_config
 from dbr.config             import GetDefaultConfigValue
 from dbr.config             import InitializeConfig
 from dbr.config             import ReadConfig
-from dbr.dialogs            import FirstRun
 from dbr.dialogs            import ShowErrorDialog
+from dbr.firstrun           import FirstRun
 from dbr.language           import GT
 from dbr.language           import LOCALE_DIR
 from dbr.language           import TRANSLATION_DOMAIN
