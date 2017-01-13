01e85babe6d5d71e16e3509ad273c1d8f5c55d57
AntumDeluge <antumdeluge@gmail.com>
2017-01-13 00:45:05 -0800

    Rename 'globals.commands' to 'globals.execute':
    Avoids conflict with Python built-in commands module.

--- dbr/tree.py
+++ dbr/tree.py
@@ -19,9 +19,9 @@ from dbr.language           import GT
 from dbr.log                import Logger
 from dbr.panel              import BorderedPanel
 from globals                import ident
-from globals.commands       import ExecuteCommand
-from globals.commands       import GetExecutable
 from globals.devices        import GetMountedStorageDevices
+from globals.execute        import ExecuteCommand
+from globals.execute        import GetExecutable
 from globals.mime           import GetFileMimeType
 from globals.paths          import ConcatPaths
 from globals.paths          import PATH_home
