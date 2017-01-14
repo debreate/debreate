575d90a40960f0a58a0e6ea66af45c761e3d059e
AntumDeluge <antumdeluge@gmail.com>
2017-01-13 23:18:43 -0800

    wiz_bin.depends: Merge some changes from 0.7 branch

--- wiz_bin/depends.py
+++ wiz_bin/depends.py
@@ -15,9 +15,11 @@ from dbr.buttons            import ButtonClear
 from dbr.buttons            import ButtonPreview64
 from dbr.buttons            import ButtonRemove
 from dbr.buttons            import ButtonSave64
+from dbr.dialogs            import ConfirmationDialog
 from dbr.help               import HelpButton
 from dbr.language           import GT
 from dbr.listinput          import ListCtrlPanel
+from dbr.log                import DebugEnabled
 from dbr.log                import Logger
 from dbr.panel              import BorderedPanel
 from dbr.wizard             import WizardPage
@@ -25,9 +27,10 @@ from globals                import ident
 from globals.strings        import TextIsEmpty
 from globals.tooltips       import SetPageToolTips
 from globals.wizardhelper   import GetPage
+from globals.wizardhelper   import GetTopWindow
 
 
-## TODO: Doxygen
+## Page defining dependencies
 class Panel(WizardPage):
     def __init__(self, parent):
         WizardPage.__init__(self, parent, ident.DEPENDS)
@@ -53,7 +56,7 @@ class Panel(WizardPage):
             u'<<',
             )
         
-        self.sel_operator = wx.Choice(self, choices=opts_operator)
+        self.sel_operator = wx.Choice(self, choices=opts_operator, name=u'operator')
         self.sel_operator.default = 0
         self.sel_operator.SetSelection(self.sel_operator.default)
         
@@ -120,60 +123,64 @@ class Panel(WizardPage):
         
         # *** Layout *** #
         
+        LEFT_BOTTOM = wx.ALIGN_LEFT|wx.ALIGN_BOTTOM
+        
         lyt_top = wx.GridBagSizer()
         
         # Row 1
-        lyt_top.Add(txt_package, pos=(0, 0), flag=wx.ALIGN_BOTTOM, border=0)
-        lyt_top.Add(txt_version, pos=(0, 2), flag=wx.ALIGN_BOTTOM)
+        lyt_top.Add(txt_package, (1, 0), flag=LEFT_BOTTOM)
+        lyt_top.Add(txt_version, (1, 2), flag=LEFT_BOTTOM)
         lyt_top.Add(btn_open, (0, 3), (4, 1), wx.ALIGN_RIGHT)
         lyt_top.Add(btn_save, (0, 4), (4, 1))
         lyt_top.Add(btn_preview, (0, 5), (4, 1))
         
         # Row 2
-        lyt_top.Add(self.ti_package, pos=(1, 0), flag=wx.ALIGN_CENTER_VERTICAL)
-        lyt_top.Add(self.sel_operator, pos=(1, 1))
-        lyt_top.Add(self.ti_version, pos=(1, 2), flag=wx.ALIGN_CENTER_VERTICAL)
+        lyt_top.Add(self.ti_package, (2, 0), flag=wx.ALIGN_CENTER_VERTICAL)
+        lyt_top.Add(self.sel_operator, (2, 1), flag=wx.ALIGN_CENTER_VERTICAL)
+        lyt_top.Add(self.ti_version, (2, 2), flag=wx.ALIGN_CENTER_VERTICAL)
         
         layout_H1 = wx.BoxSizer(wx.HORIZONTAL)
         layout_H1.Add(lyt_top, 0, wx.ALIGN_BOTTOM)
         layout_H1.AddStretchSpacer(1)
         layout_H1.Add(btn_help)
         
-        layout_categories = wx.GridSizer(4, 2, 5, 5)
+        lyt_categories = wx.GridSizer(4, 2, 5, 5)
         
         for C in self.categories:
-            layout_categories.Add(C, 0)
+            lyt_categories.Add(C, 0)
         
-        pnl_categories.SetSizer(layout_categories)
         pnl_categories.SetAutoLayout(True)
+        pnl_categories.SetSizer(lyt_categories)
         pnl_categories.Layout()
         
-        layout_buttons = wx.BoxSizer(wx.HORIZONTAL)
+        lyt_buttons = wx.BoxSizer(wx.HORIZONTAL)
         
-        layout_buttons.AddMany( (
+        lyt_buttons.AddMany((
             (btn_add, 0, wx.ALIGN_CENTER_VERTICAL),
             (btn_append, 0, wx.ALIGN_CENTER_VERTICAL),
             (btn_remove, 0, wx.ALIGN_CENTER_VERTICAL),
             (btn_clear, 0, wx.ALIGN_CENTER_VERTICAL),
-            ) )
+            ))
         
-        layout_G2 = wx.GridBagSizer(5, 5)
-        layout_G2.SetCols(2)
+        lyt_mid = wx.GridBagSizer()
+        lyt_mid.SetCols(2)
         
-        layout_G2.Add(wx.StaticText(self, label=u'Categories'), (0, 0), (1, 1))
-        layout_G2.Add(pnl_categories, (1, 0), flag=wx.RIGHT, border=5)
-        layout_G2.Add(layout_buttons, (1, 1), flag=wx.ALIGN_BOTTOM)
+        lyt_mid.Add(wx.StaticText(self, label=u'Categories'), (0, 0), (1, 1), LEFT_BOTTOM)
+        lyt_mid.Add(pnl_categories, (1, 0), flag=wx.RIGHT, border=5)
+        lyt_mid.Add(lyt_buttons, (1, 1), flag=wx.ALIGN_BOTTOM)
         
-        layout_H3 = wx.BoxSizer(wx.HORIZONTAL)
-        layout_H3.Add(self.lst_deps, 1, wx.EXPAND)
+        lyt_list = wx.BoxSizer(wx.HORIZONTAL)
+        lyt_list.Add(self.lst_deps, 1, wx.EXPAND)
         
-        layout_main = wx.BoxSizer(wx.VERTICAL)
-        layout_main.Add(layout_H1, 0, wx.EXPAND|wx.ALL, 5)
-        layout_main.Add(layout_G2, 0, wx.ALL, 5)
-        layout_main.Add(layout_H3, 1, wx.EXPAND|wx.ALL, 5)
+        lyt_main = wx.BoxSizer(wx.VERTICAL)
+        # Spacer is less on this page because text is aligned to bottom
+        lyt_main.AddSpacer(5)
+        lyt_main.Add(layout_H1, 0, wx.EXPAND|wx.LEFT|wx.RIGHT, 5)
+        lyt_main.Add(lyt_mid, 0, wx.LEFT|wx.RIGHT, 5)
+        lyt_main.Add(lyt_list, 1, wx.EXPAND|wx.ALL, 5)
         
         self.SetAutoLayout(True)
-        self.SetSizer(layout_main)
+        self.SetSizer(lyt_main)
         self.Layout()
     
     
@@ -189,7 +196,7 @@ class Panel(WizardPage):
     
     ## TODO: Doxygen
     def GetDefaultCategory(self):
-        return GT(u'Depends')
+        return self.default_category
     
     
     ## TODO: Doxygen
@@ -205,6 +212,14 @@ class Panel(WizardPage):
     
     ## Resets all fields on page to default values
     def ResetPage(self):
+        for C in self.categories:
+            if C.GetName() == self.default_category:
+                C.SetValue(True)
+                break
+        
+        self.ti_package.Clear()
+        self.sel_operator.SetSelection(self.sel_operator.default)
+        self.ti_version.Clear()
         self.lst_deps.DeleteAllItems()
     
     
@@ -239,32 +254,42 @@ class Panel(WizardPage):
         
         elif key_id == ident.APPEND:
             selected_count = self.lst_deps.GetSelectedItemCount()
+            
+            Logger.Debug(__name__, u'Appending to {} items'.format(selected_count))
+            
             if not TextIsEmpty(addname) and self.lst_deps.GetItemCount() and selected_count:
-                listrow = None
-                for X in range(selected_count):
-                    if listrow == None:
-                        listrow = self.lst_deps.GetFirstSelected()
+                selected_rows = self.lst_deps.GetSelectedIndexes()
+                
+                if DebugEnabled():
+                    Logger.Debug(__name__, u'Selected rows:')
+                    for R in selected_rows:
+                        print(u'\t{}'.format(R))
+                
+                for listrow in selected_rows:
+                    Logger.Debug(__name__, u'Setting list row: {}'.format(listrow))
                     
-                    else:
-                        listrow = self.lst_deps.GetNextSelected(listrow)
-                    
-                    colitem = self.lst_deps.GetItem(listrow, 1)  # Get item from second column
-                    prev_text = colitem.GetText()  # Get the text from that item
+                    # Get item from second column
+                    colitem = self.lst_deps.GetItem(listrow, 1)
+                    # Get the text from that item
+                    prev_text = colitem.GetText()
                     
                     if not TextIsEmpty(ver):
-                        self.lst_deps.SetStringItem(listrow, 1, u'{} | {} {}'.format(prev_text, addname, addver))
+                        new_text = u'{} | {} {}'.format(prev_text, addname, addver)
                     
                     else:
-                        self.lst_deps.SetStringItem(listrow, 1, u'{} | {}'.format(prev_text, addname))
+                        new_text = u'{} | {}'.format(prev_text, addname)
+                    
+                    Logger.Debug(__name__, u'Appended item: {}'.format(new_text))
+                    
+                    self.lst_deps.SetStringItem(listrow, 1, new_text)
         
-        elif key_id == wx.ID_REMOVE:
+        elif key_id in (wx.ID_REMOVE, wx.WXK_DELETE):
             self.lst_deps.RemoveSelected()
         
         elif key_id == wx.ID_CLEAR:
             if self.lst_deps.GetItemCount():
-                confirm = wx.MessageDialog(self, GT(u'Clear all dependencies?'), GT(u'Confirm'),
-                        wx.YES_NO|wx.NO_DEFAULT|wx.ICON_QUESTION)
-                if confirm.ShowModal() == wx.ID_YES:
+                if ConfirmationDialog(GetTopWindow(), GT(u'Confirm'),
+                        GT(u'Clear all dependencies?')).ShowModal() in (wx.ID_OK, wx.OK):
                     self.lst_deps.DeleteAllItems()
         
         if event:
