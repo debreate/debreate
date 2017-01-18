51487af7d2bf5bc1004e7f00dab3a7d63e7e3ac2
AntumDeluge <antumdeluge@gmail.com>
2017-01-18 10:13:11 -0800

    ui.panel: Add ScrolledPanel class:
    A wx.ScrolledWindow that sets scrollbars by default.

--- ui/panel.py
+++ ui/panel.py
@@ -18,3 +18,12 @@ class BorderedPanel(wx.Panel):
     def __init__(self, parent, ID=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize,
                 style=wx.TAB_TRAVERSAL, name=wx.PanelNameStr):
         wx.Panel.__init__(self, parent, ID, pos, size, style|PANEL_BORDER, name)
+
+
+## A wx.ScrolledWindow that sets scrollbars by default
+class ScrolledPanel(wx.ScrolledWindow):
+    def __init__(self, parent, win_id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize,
+                style=wx.HSCROLL|wx.VSCROLL, name=u'scrolledPanel'):
+        wx.ScrolledWindow.__init__(self, parent, win_id, pos, size, style, name)
+        
+        self.SetScrollbars(0, 20, 0, 0)
