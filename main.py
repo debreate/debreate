33836de0e2b08e3c371bb0c890ca30d5fb192dbb
AntumDeluge <antumdeluge@gmail.com>
2017-01-20 21:12:57 -0800

    Rename 'wiz_bin.man' to 'wiz_bin.manuals':
    Affects main

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ from wiz_bin.depends        import Panel as PageDepends
 from wiz_bin.files          import Panel as PageFiles
 from wiz_bin.greeting       import Panel as PageGreeting
 from wiz_bin.launchers      import Panel as PageLaunchers
-from wiz_bin.man            import Panel as PageMan
+from wiz_bin.manuals        import Panel as PageMan
 from wiz_bin.scripts        import Panel as PageScripts
 
 
