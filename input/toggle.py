3c035b737deb938642aa52fe2174e23d783a0336
AntumDeluge <antumdeluge@gmail.com>
2017-01-26 21:09:23 -0800

    input.toggle: Add class CheckBoxCFG:
    - CheckBox that updates config file when value is changed

--- input/toggle.py
+++ input/toggle.py
@@ -9,6 +9,7 @@
 import wx
 
 from globals.wizardhelper   import FieldEnabled
+from input.cfgfield         import ConfigField
 from input.essential        import EssentialField
 
 
@@ -55,6 +56,15 @@ class CheckBox(wx.CheckBox):
     ## TODO: Doxygen
     def SetDefaultValue(self, value):
         self.default = value
+
+
+## CheckBox that updates config file when value is changed
+class CheckBoxCFG(CheckBox, ConfigField):
+    def __init__(self, parent, win_id=wx.ID_ANY, label=wx.EmptyString, pos=wx.DefaultPosition,
+                size=wx.DefaultSize, style=0, name=wx.CheckBoxNameStr, cfgKey=None):
+        
+        CheckBox.__init__(self, parent, win_id, label, pos, size, style, name)
+        ConfigField.__init__(self, cfgKey)
 
 
 ## CheckBox class that notifies main window to mark project dirty
