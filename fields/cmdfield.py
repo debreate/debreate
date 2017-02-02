51f965672beaa4dfe409de554598c2ef45ae699a
AntumDeluge <antumdeluge@gmail.com>
2017-02-01 21:59:00 -0800

    fields.cmdfield.CommandField: Class changes:
    - Constructor:
      - Set 'commands' argument default to 'None'
      - Allow 'commands' to be string separated with whitespace
      - Add 'requireAll' argument
    - Check method:
      - Only check if self.Commands is a 'true' value
      - Change behavior if 'RequireAll' set to 'True'

--- fields/cmdfield.py
+++ fields/cmdfield.py
@@ -6,14 +6,21 @@
 # See: docs/LICENSE.txt
 
 
-from globals.execute import GetExecutable
-from globals.strings import IsString
+from globals.cmdcheck   import CommandExists
+from globals.strings    import IsString
+from globals.strings    import RemoveEmptyLines
 
 
 ## A field that requires a specific command or commands to be available on the system
 class CommandField:
-    def __init__(self, commands):
+    def __init__(self, commands=None, requireAll=False):
         self.Commands = commands
+        self.RequireAll = requireAll
+        
+        if commands:
+            if IsString(self.Commands) and u' ' in self.Commands:
+                self.Commands = self.Commands.split(u' ')
+                self.Commands = RemoveEmptyLines(self.Commands)
         
         # Check for the commands when the object is constructed
         self.Check()
@@ -21,15 +28,30 @@ class CommandField:
     
     ## Disables the field if a command not found on system
     def Check(self):
-        if IsString(self.Commands):
-            self.Enable(GetExecutable(self.Commands))
+        if self.Commands:
+            enabled = True
             
-            return
-        
-        for CMD in self.Commands:
-            if not GetExecutable(CMD):
-                self.Disable()
+            if IsString(self.Commands):
+                # FIXME: CommandExists should return boolean
+                #self.Enable(not not CommandExists(self.Commands))
+                enabled = not not CommandExists(self.Commands)
+            
+            else:
+                if self.RequireAll:
+                    for CMD in self.Commands:
+                        if not CommandExists(CMD):
+                            enabled = False
+                            
+                            break
                 
-                return
-        
-        self.Enable()
+                else:
+                    cmd_found = False
+                    for CMD in self.Commands:
+                        if CommandExists(CMD):
+                            cmd_found = True
+                            
+                            break
+                    
+                    enabled = cmd_found
+            
+            self.Enable(enabled)
