42eeb8f6d51c58b67b43f991a95593925fd00583
AntumDeluge <antumdeluge@gmail.com>
2017-02-02 00:29:56 -0800

    wiz_bin.copyright: Rename method 'SetCopyright' to 'Set':
    Affects:
    - main

--- wiz_bin/copyright.py
+++ wiz_bin/copyright.py
@@ -285,7 +285,7 @@ class Panel(WizardPage):
     
     
     ## Sets the text of the displayed copyright
-    def SetCopyright(self, data):
+    def Set(self, data):
         self.dsp_copyright.SetValue(data)
     
     
