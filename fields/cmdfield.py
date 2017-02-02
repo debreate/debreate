10007adbb5449c34c50a42a5d4fe332b92eaba8f
AntumDeluge <antumdeluge@gmail.com>
2017-02-01 20:29:29 -0800

    Add 'fields.cmdfield' module:
    - Defines CommandField class
      - A field that requires a specific command or commands to be available
    on the system

---
+++ fields/cmdfield.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from globals.execute import GetExecutable
+from globals.strings import IsString
+
+## \package fields.cmdfield
+
+# MIT licensing
+# See: docs/LICENSE.txt
+
+
+## A field that requires a specific command or commands to be available on the system
+class CommandField:
+    def __init__(self, commands):
+        self.Commands = commands
+        
+        # Check for the commands when the object is constructed
+        self.Check()
+    
+    
+    ## Disables the field if a command not found on system
+    def Check(self):
+        if IsString(self.Commands):
+            self.Enable(GetExecutable(self.Commands))
+            
+            return
+        
+        for CMD in self.Commands:
+            if not GetExecutable(CMD):
+                self.Disable()
+                
+                return
+        
+        self.Enable()
