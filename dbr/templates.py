6465219d17ff7718c64fcb19c5ec795608996e5e
AntumDeluge <antumdeluge@gmail.com>
2017-02-03 17:45:06 -0800

    dbr.templates: Move GetLicenses to globals.fileio & rename to 'GetFiles'

--- dbr/templates.py
+++ dbr/templates.py
@@ -10,6 +10,7 @@ import os
 
 from dbr.language       import GT
 from dbr.log            import Logger
+from globals.fileio     import GetFiles
 from globals.paths      import ConcatPaths
 from globals.paths      import PATH_app
 from globals.paths      import PATH_local
@@ -45,45 +46,24 @@ local_templates_path = ConcatPaths((PATH_local, u'templates'))
 local_licenses_path = ConcatPaths((local_templates_path, u'licenses'))
 
 
-## Retrieves all files from the given path
-#
-#  FIXME: Should be renamed to "GetFiles" & moved to a "files" or "general" module
-#
-#  \param path
-#    Directory to search for license templates
-def GetLicenses(path):
-    licenses = []
-    
-    for PATH, DIRS, FILES in os.walk(path):
-        for F in FILES:
-            license_path = ConcatPaths((path, F))
-            
-            if os.path.isfile(license_path):
-                licenses.append(F)
-                
-                Logger.Debug(__name__, u'Loaded license: {}'.format(license_path))
-    
-    return sorted(licenses, key=GS.lower)
-
-
 ## Retrieves a list of licenses installed on the system
 #  
 #  Common system license files are located in /usr/share/common-licenses.
 def GetSysLicenses():
-    return GetLicenses(sys_licenses_path)
+    return GetFiles(sys_licenses_path)
 
 
 ## Retrieves a list of licenses located under the ".local" directory of the user's home path
 def GetLocalLicenses():
-    return GetLicenses(local_licenses_path)
+    return GetFiles(local_licenses_path)
 
 
 ## Join the app & local licenses lists
 def GetCustomLicenses():
     # Local licenses take priority
-    licenses = GetLicenses(local_licenses_path)
+    licenses = GetFiles(local_licenses_path)
     
-    for LIC in GetLicenses(app_licenses_path):
+    for LIC in GetFiles(app_licenses_path):
         if LIC not in licenses:
             licenses.append(LIC)
     
@@ -106,10 +86,10 @@ def GetLicenseTemplateFile(l_name):
     template_path = None
     
     # Check local templates first
-    if l_name in GetLicenses(local_licenses_path):
+    if l_name in GetFiles(local_licenses_path):
         template_path = ConcatPaths((local_licenses_path, l_name))
     
-    elif l_name in GetLicenses(app_licenses_path):
+    elif l_name in GetFiles(app_licenses_path):
         template_path = ConcatPaths((app_licenses_path, l_name))
     
     if not template_path or not os.path.isfile(template_path):
