3b287172a1b6ace516538ec67c1a3bab649cbe7a
AntumDeluge <antumdeluge@gmail.com>
2017-02-03 16:22:26 -0800

    dbr.templates: Function changes:
    - Rename 'Set*LicenseTemplates' to 'Get*LicenseTemplates' & return
    sorted lists instead of setting global lists
    - GetLicenseTemplateFile
      - Rename 'l_template' to 'template_path'
      - Call 'Get' functions instead of using global lists
      - Use 'ConcatPaths' function
    - GetLicenseTemplateList
      - Call 'Get' functions instead of using global lists
      - Use 'globals.strings.GS' instead of 'unicode' to sort returned list

--- dbr/templates.py
+++ dbr/templates.py
@@ -8,10 +8,12 @@
 
 import os
 
-from dbr.language   import GT
-from dbr.log        import Logger
-from globals.paths  import PATH_app
-from globals.paths  import PATH_local
+from dbr.language       import GT
+from dbr.log            import Logger
+from globals.paths      import ConcatPaths
+from globals.paths      import PATH_app
+from globals.paths      import PATH_local
+from globals.strings    import GS
 
 
 ## Application templates
@@ -38,16 +40,9 @@ local_templates_path = u'{}/templates'.format(PATH_local)
 #  <templates_path>/licenses
 local_licenses_path = u'{}/licenses'.format(local_templates_path)
 
-licenses_app = []
-licenses_local = []
-
 
 ## Initializes/Refreshes the app licenses list
-#
-#  FIXME: Rename to "GetAppLicenseTemplates" & don't set global list
-def SetAppLicenseTemplates():
-    global licenses_app
-    
+def GetAppLicenseTemplates():
     # Clear old values
     licenses_app = []
     
@@ -57,14 +52,12 @@ def SetAppLicenseTemplates():
                 licenses_app.append(F)
                 
                 Logger.Debug(__name__, u'Loaded global license: {}'.format(F))
+    
+    return sorted(licenses_app, key=GS.lower)
 
 
 ## Initializes/Refreshes the local licenses list
-#
-#  FIXME: Rename to "GetLocalLicenseTemplates" & don't set global list
-def SetLocalLicenseTemplates():
-    global licenses_local
-    
+def GetLocalLicenseTemplates():
     # Clear old values
     licenses_local = []
     
@@ -74,35 +67,40 @@ def SetLocalLicenseTemplates():
                 licenses_local.append(F)
                 
                 Logger.Debug(__name__, u'Loaded local license: {}'.format(F))
+    
+    return sorted(licenses_local, key=GS.lower)
 
 
 ## Retrieves the absolute path of a license template
-#  
+#
+#  FIXME: Rename to "GetLicenseTemplatePath"
 #  Templates are checked for first in local directory
 #    (<HOME>/.local/share/debreate/templates/licenses)
 #    then in the application's directory
 #    (<PATH_app>/templates/licenses).
+#
 #  \param l_name
 #        The filename of the template
 #  \return
 #        Absolute path of template file
 def GetLicenseTemplateFile(l_name):
-    l_template = None
+    template_path = None
     
     # Check local templates first
-    if l_name in licenses_local:
-        l_template = u'{}/{}'.format(local_licenses_path, l_name)
+    if l_name in GetLocalLicenseTemplates():
+        template_path = ConcatPaths((local_licenses_path, l_name))
     
-    elif l_name in licenses_app:
-        l_template = u'{}/{}'.format(application_licenses_path, l_name)
+    elif l_name in GetAppLicenseTemplates():
+        template_path = ConcatPaths((application_licenses_path, l_name))
     
+    if not template_path or not os.path.isfile(template_path):
+        Logger.Warn(__name__, GT(u'License template not found: {}'.format(template_path)))
+        
+        return
     
-    if os.path.isfile(l_template):
-        Logger.Info(__name__, GT(u'Loading license template: {}'.format(l_template)))
-    else:
-        Logger.Warn(__name__, GT(u'License template not found: {}'.format(l_template)))
+    Logger.Info(__name__, GT(u'Loading license template: {}'.format(template_path)))
     
-    return l_template
+    return template_path
 
 
 ## Function to retrieve available license templates
@@ -111,17 +109,13 @@ def GetLicenseTemplateFile(l_name):
 #  Then retrieved from <PATH_app>/templates/licenses. Only files that
 #  don't already exist in local path are added.
 def GetLicenseTemplatesList():
-    # Refresh lists of available templates
-    SetAppLicenseTemplates()
-    SetLocalLicenseTemplates()
-    
     # Use local templates first if available
-    templates_list = licenses_local[:]
+    templates_list = GetLocalLicenseTemplates()
     
     # Retrieve licenses that are not available from local path
-    for LIC in licenses_app:
+    for LIC in GetAppLicenseTemplates():
         if LIC not in templates_list:
             templates_list.append(LIC)
     
     # Return an alphabetically sorted list
-    return sorted(templates_list, key=unicode.lower)
+    return sorted(templates_list, key=GS.lower)
