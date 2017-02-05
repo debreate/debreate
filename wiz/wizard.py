0d5761bd65166559d22f79dc65e72c95fe45950d
AntumDeluge <antumdeluge@gmail.com>
2017-02-04 22:55:36 -0800

    wiz.wizard.Wizard: Changes to 'Initialize' method:
    - Call 'InitPage' on all loaded pages

--- wiz/wizard.py
+++ wiz/wizard.py
@@ -321,6 +321,9 @@ class Wizard(wx.Panel):
         else:
             self.ShowPage(self.pages[showPage].Id)
         
+        for PAGE in self.pages:
+            PAGE.InitPage()
+        
         self.Layout()
     
     
