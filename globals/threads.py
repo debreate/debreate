24d16fbe068d62de669de8ecb42ba8f6cc64a42a
AntumDeluge <antumdeluge@gmail.com>
2017-02-06 13:21:23 -0800

    globals.threads.Thread: Override 'join' method & add aliases:
    - 'Join' & 'Exit'

--- globals/threads.py
+++ globals/threads.py
@@ -30,6 +30,13 @@ class Thread(thr.Thread):
         Logger.Debug(__name__, u'Destroying Thread instance; Thread is active: {}'.format(self.IsActive()))
     
     
+    ## Exits the thread & sets inactive
+    #
+    #  Alias of globals.threads.Thread.Join
+    def Exit(self):
+        return self.Join()
+    
+    
     ## Retrieves the thread identifier
     def GetId(self):
         return self.ident
@@ -37,6 +44,23 @@ class Thread(thr.Thread):
     ## Tests if thread is active
     def IsActive(self):
         return self.Active
+    
+    
+    ## Exits the thread & sets inactive
+    def join(self):
+        if self.IsActive():
+            Logger.Debug(__name__, u'Joining thread ...')
+            
+            thr.Thread.join(self)
+            self.Active = False
+    
+    
+    ## Exits the thread & sets inactive
+    #
+    #  Alias of globals.threads.Thread.join
+    def Join(self):
+        return self.join()
+    
     
     ## Alias for start method
     def Start(self):
