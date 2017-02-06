875dc3c3bf2971541456b80fa7d20ccc5fae196e
AntumDeluge <antumdeluge@gmail.com>
2017-02-06 10:40:25 -0800

    globals.filio.FileItem: Class & method changes:
    - Add 'Timestamp' member attribute
    - Add methods:
      - 'IsFile'
        - Checks if path is a regular file
      - 'Read'
        - Opens path & reads text contents
      - 'GetTimestamp'
      	- Returns the saved timestamp
      - 'TimestampChanged'
        - Checks the current file's timestamp against the saved one
    - In 'IsExecutable' method, check if path is a regular file before
    checking executable permission

--- globals/fileio.py
+++ globals/fileio.py
@@ -21,6 +21,9 @@ class FileItem:
     def __init__(self, path, target=None):
         self.Path = path
         self.Target = target
+        
+        # Timestamp is set at construction
+        self.Timestamp = GetTimestamp(self.Path)
     
     
     ## Checks if the file exists on the filesystem
@@ -43,6 +46,14 @@ class FileItem:
         return self.Target
     
     
+    ## Retrieves the file's timestamp from memory
+    #
+    #  NOTE: May differ from actual timestamp
+    #        Call 'TimestampChanged' to update
+    def GetTimestamp(self):
+        return self.Timestamp
+    
+    
     ## Checks if the file has a target installation directory
     def HasTarget(self):
         return IsString(self.Target) and not TextIsEmpty(self.Target)
@@ -53,9 +64,30 @@ class FileItem:
         return os.path.isdir(self.Path)
     
     
+    ## Checks if the item represented is a regular file
+    def IsFile(self):
+        return os.path.isfile(self.Path)
+    
+    
     ## Checks if file is executable
     def IsExecutable(self):
-        return os.access(self.Path, os.X_OK)
+        if self.IsFile():
+            return os.access(self.Path, os.X_OK)
+        
+        return False
+    
+    
+    ## Reads file's contents into memory
+    #
+    #  \param split
+    #    If \b \e True, splits the text into a list
+    #  \param convert
+    #    Type of list to split contents into (can be \b \e tuple or \b \e list)
+    #    FIXME: Use boolean???
+    #  \param noStrip
+    #    \b \e String of leading & trailing characters to not strip
+    def Read(self, split=False, convert=tuple, noStrip=None):
+        return ReadFile(self.Path, split, convert, noStrip)
     
     
     ## Sets file's path & basename
@@ -66,6 +98,25 @@ class FileItem:
     ## Sets file's target directory
     def SetTarget(self, target):
         self.Target = target
+    
+    
+    ## Checks if timestamp has been modified & updates
+    def TimestampChanged(self):
+        # Set file's timestamp if not already done
+        if not self.Timestamp:
+            self.Timestamp = GetTimestamp(self.Path)
+            
+            return False
+        
+        current_stamp = GetTimestamp(self.Path)
+        
+        if current_stamp != self.Timestamp:
+            self.Timestamp = current_stamp
+            
+            return True
+        
+        return False
+            
 
 
 ## Append text to end of a file
