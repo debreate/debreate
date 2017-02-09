2ef45489f8465a1a836434022d3d907abcafc389
AntumDeluge <antumdeluge@gmail.com>
2017-02-09 05:45:56 -0800

    dbr.app: Add some Doxygen notes

--- dbr/app.py
+++ dbr/app.py
@@ -11,23 +11,35 @@ import wx
 
 ## Custom wx.App class for setting & retrieving main window instance
 class DebreateApp(wx.App):
+    ## Constructor
     def __init__(self):
         wx.App.__init__(self)
         
         self.MainWindow = None
     
     
-    ## Retrieves main window instance
+    ## Retrieves the main window
+    #
+    #  \return
+    #    <b><i>main.MainWindow</i></b> instance
     def GetMainWindow(self):
         return self.MainWindow
     
     
-    ## Retrieves the wiz.wizard.Wizard instance
+    ## Retrieves the wizard
+    #
+    #  \return
+    #    <b><i>wiz.wizard.Wizard</i></b> instance
     def GetWizard(self):
         if self.MainWindow:
             return self.MainWindow.GetWizard()
     
     
     ## Set the main window instance
+    #
+    #  \param window
+    #    <b><i>wx.Frame</i></b> instance to use for main window
+    #  \return
+    #    <b><i>None</i></b>
     def SetMainWindow(self, window):
         self.MainWindow = window
