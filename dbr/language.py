638627018667e02e814c9e0cf2860b792ae9df58
AntumDeluge <antumdeluge@gmail.com>
2017-02-09 07:39:31 -0800

    Rename 'LOCALE_DIR' to 'DIR_locale'

--- dbr/language.py
+++ dbr/language.py
@@ -12,9 +12,9 @@ from globals.strings import GS
 
 
 TRANSLATION_DOMAIN = u'debreate'
-LOCALE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), u'locale')
+DIR_locale = os.path.join(os.path.dirname(os.path.dirname(__file__)), u'locale')
 
-gettext.install(TRANSLATION_DOMAIN, LOCALE_DIR, unicode=True)
+gettext.install(TRANSLATION_DOMAIN, DIR_locale, unicode=True)
 
 
 ## Ensure gettext 
