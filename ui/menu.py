80ae2ae4259c2dac69a3312a0733c5fda2b3b558
AntumDeluge <antumdeluge@gmail.com>
2017-02-09 21:11:30 -0800

    ui.menu: Add Doxygen docs & rename some method args

--- ui/menu.py
+++ ui/menu.py
@@ -14,13 +14,14 @@ from wiz.helper import GetMainWindow
 
 
 ## A menu bar that stores an ID along with a menu
-#  
-#  \param parent
-#    \b \e wx.Window : Parent window of the menu bar
-#        If not None, automatically sets itself as parent's menu bar
-#  \param style
-#    \b \e int : Menu bar style
 class MenuBar(wx.MenuBar):
+    ## Constructor
+    #
+    #  \param parent
+    #    <b><i>wx.Window</i></b> parent window of the menu bar
+    #    If not None, automatically sets itself as parent's menu bar
+    #  \param style
+    #    Menu bar style represented by an <b><i>integer</i></b> value
     def __init__(self, parent=None, style=0):
         wx.MenuBar.__init__(self, style)
         
@@ -33,25 +34,25 @@ class MenuBar(wx.MenuBar):
     ## Append a menu to the end of menu bar
     #
     #  \param menu
-    #    \b \e wx.Menu : Menu to be appended
+    #    <b><i>wx.Menu</i></b> instance to be appended
     #  \param title
-    #    \b \e unicode|str : Text to be displayed in the menu bar
-    #  \param ID
-    #    \b \e int : ID to store for menu
-    def Append(self, menu, title, ID):
-        self.id_list.append(ID)
+    #    Label to be displayed in the menu bar
+    #  \param menuId
+    #    Unique <b><i>integer</i></b> identifier to store for menu
+    def Append(self, menu, title, menuId):
+        self.id_list.append(menuId)
         
         return wx.MenuBar.Append(self, menu, title)
     
     
     ## Finds a wx.Menu by ID
     #
-    #  \param ID
-    #    \b \e int : ID to search for in menu bar
+    #  \param menuId
+    #    Menu <b><i>integer</i></b> identifier to search for in menu bar
     #  \return
-    #    The wx.Menu with ID
-    def GetMenuById(self, ID):
-        m_index = self.id_list.index(ID)
+    #    The <b><i>wx.Menu</i></b> with using identifier
+    def GetMenuById(self, menuId):
+        m_index = self.id_list.index(menuId)
         
         return self.GetMenu(m_index)
     
@@ -59,26 +60,36 @@ class MenuBar(wx.MenuBar):
     ## Insert a menu to a specified position in the menu bar
     #
     #  \param index
-    #    \b \e int : Position index to insert menu
+    #    Position index to insert menu
     #  \param menu
-    #    \b \e wx.Menu : Menu to be inserted
+    #    <b><i>wx.Menu</i></b> instance to be inserted
     #  \param title
-    #    \b \e unicode|str : Text to be displayed in the menu bar
-    #  \param ID
-    #    \b \e int : ID to store for menu
-    def Insert(self, index, menu, title, ID):
-        self.id_list.insert(index, ID)
+    #    Label to be displayed in the menu bar
+    #  \param menuId
+    #    Unique <b><i>integer</i></b> identifier to store for menu
+    def Insert(self, index, menu, title, menuId):
+        self.id_list.insert(index, menuId)
         
         return wx.MenuBar.Insert(self, index, menu, title)
 
 
 ## A custom menu designed for use with PanelMenuBar
 class PanelMenu(wx.StaticText):
-    def __init__(self, parent=None, win_id=wx.ID_ANY, label=wx.EmptyString, name=u'menu'):
+    ## Constructor
+    #
+    #  \param parent
+    #    <b><i>wx.Window</i></b> parent instance
+    #  \param winId
+    #    <b><i>Integer</i></b> identifier for menu item
+    #  \param label
+    #    Text label displayed for menu item
+    #  \param name
+    #    The menu's name attribute
+    def __init__(self, parent=None, winId=wx.ID_ANY, label=wx.EmptyString, name=u'menu'):
         if not parent:
             parent = GetMainWindow()
         
-        wx.StaticText.__init__(self, parent, win_id, label, name=name)
+        wx.StaticText.__init__(self, parent, winId, label, name=name)
         
         self.fg_orig = self.GetForegroundColour()
         
@@ -95,9 +106,14 @@ class PanelMenu(wx.StaticText):
         self.Bind(wx.EVT_MENU_CLOSE, self.OnMenuClose)
     
     
-    ## TODO: Doxygen
-    def Append(self, menu_id, label):
-        self.Menu.Append(menu_id, label)
+    ## Append an item the the menu
+    #
+    #  \param menuId
+    #    Unique <b><i>integer</i></b> identifier
+    #  \param label
+    #    Text label displayed for menu item
+    def Append(self, menuId, label):
+        self.Menu.Append(menuId, label)
     
     
     ## Checks if menu is in selected state
@@ -105,7 +121,7 @@ class PanelMenu(wx.StaticText):
         return self.Selected
     
     
-    ## TODO: doxygen
+    ## Handles action to take when a menu is closed/collapsed
     def OnMenuClose(self, event=None):
         self.Select(False)
         
@@ -113,7 +129,7 @@ class PanelMenu(wx.StaticText):
             event.Skip()
     
     
-    ## TODO: Doxygen
+    ## Passes the event to be handled by the parent instance
     def OnSelect(self, event=None):
         if event:
             # Send event to parent PanelMenuBar instance
@@ -144,8 +160,14 @@ class PanelMenu(wx.StaticText):
 
 ## A custom menu bar designed for use in wx.Panel
 class PanelMenuBar(BorderedPanel):
-    def __init__(self, parent, win_id=wx.ID_ANY, name=u'menubar'):
-        BorderedPanel.__init__(self, parent, win_id, name=name)
+    ## Constructor
+    #
+    #  \param parent
+    #    <b><i>wx.Window</i></b> parent instance
+    #  \param winId
+    #    <b><i>Integer</i></b> identifier used for menu bar
+    def __init__(self, parent, winId=wx.ID_ANY, name=u'menubar'):
+        BorderedPanel.__init__(self, parent, winId, name=name)
         
         self.Padding = 5
         
@@ -165,19 +187,19 @@ class PanelMenuBar(BorderedPanel):
     #
     #  \param label
     #    Text displayed for menu
-    #  \param win_id
-    #    ID to use for menu
-    def Add(self, label, win_id=wx.ID_ANY):
-        new_menu = PanelMenu(self, win_id, label)
+    #  \param winId
+    #    <b><i>Integer</i></b> identifier to use for menu
+    def Add(self, label, winId=wx.ID_ANY):
+        new_menu = PanelMenu(self, winId, label)
         self.AddItem(new_menu)
     
     
     ## Add a PanelMenu instance to menu bar
     #
     #  \param menu
-    #    \b \e PanelMenu instance to be added
+    #    ui.menu.PanelMenu instance to be added
     #  \param label
-    #    Optional \b \e string label to override menu label
+    #    Optional <b><i>string</i></b> label to override menu label
     def AddItem(self, menu, label=None):
         if label:
             menu.SetLabel(label)
@@ -192,8 +214,8 @@ class PanelMenuBar(BorderedPanel):
     
     
     ## Alias for ui.menu.PanelMenuBar.Add
-    def Append(self, label, win_id=wx.ID_ANY):
-        self.Add(label, win_id)
+    def Append(self, label, winId=wx.ID_ANY):
+        self.Add(label, winId)
     
     
     ## Alias for ui.menu.PanelMenuBar.AddItem
@@ -203,10 +225,10 @@ class PanelMenuBar(BorderedPanel):
     
     ## Find a menu within the menu bar that matches a given ID
     #
-    #  \param win_id
-    #    \b \e Integer ID to scan for
+    #  \param winId
+    #    <b><i>Integer</i></b> identifier to scan for
     #  \return
-    #    \b \e PanelMenu instance matching win_id or None
+    #    ui.menu.PanelMenu instance matching winId or <b><i>None</i></b>
     def GetMenuById(self, win_id):
         for M in self.GetMenuList():
             if M.GetId() == win_id:
@@ -216,9 +238,9 @@ class PanelMenuBar(BorderedPanel):
     ## Find a menu within the menu bar at given index
     #
     #  \param index
-    #    Index of item to return
+    #    <b><i>Integer</i></b> index of menu item to return
     #  \return
-    #    \b \e PanelMenu at menu index
+    #    <b><i>ui.menu.PanelMenu</i></b> instance at menu index
     def GetMenuByIndex(self, index):
         return self.GetMenuList()[index]
     
@@ -226,9 +248,9 @@ class PanelMenuBar(BorderedPanel):
     ## Find a menu within the menu bar that matches a given label
     #
     #  \param label
-    #    \b \e String label to scan for
+    #    Text label to scan for
     #  \return
-    #    \b \e PanelMenu instance with matching label or None
+    #    ui.menu.PanelMenu instance with matching label or <b><i>None</i></b>
     def GetMenuByLabel(self, label):
         for M in self.GetMenuList():
             if M.GetLabel() == label:
@@ -236,6 +258,11 @@ class PanelMenuBar(BorderedPanel):
     
     
     ## Find the index within the menu bar of a give item
+    #
+    #  \param menu
+    #    The ui.menu.PanelMenu instance
+    #  \return
+    #    <b><i>Integer</i></b> index of panel menu
     def GetMenuIndex(self, menu):
         if menu == None:
             return None
@@ -255,8 +282,9 @@ class PanelMenuBar(BorderedPanel):
     ## Retrieves a standard list of all menus in menu bar
     #
     #  FIXME: Need a failsafe for non-PanelMenu objects???
+    #
     #  \return
-    #    \b \e Tuple list of found menus
+    #    <b><i>Tuple</i></b> list of found menus
     def GetMenuList(self):
         menu_list = []
         
@@ -273,6 +301,7 @@ class PanelMenuBar(BorderedPanel):
     ## Action to take when menu is selected
     #
     #  FIXME: Needs to find the menu item that was selected
+    #  TODO: Define
     def OnSelect(self, event=None):
         if event:
             # DEBUG: Start
