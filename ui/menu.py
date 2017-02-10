c02454192ba80588bfa338f0154ed73401e2b00c
AntumDeluge <antumdeluge@gmail.com>
2017-02-09 21:11:30 -0800

    ui.menu: Add Doxygen docs & rename some method args
    Cherry pick conflicts:
    	ui/menu.py

--- ui/menu.py
+++ ui/menu.py
@@ -10,13 +10,14 @@ import wx
 
 
 ## A menu bar that stores an ID along with a menu
-#  
-#  \param parent
-#    \b \e wx.Window : Parent window of the menu bar
-#        If not None, automatically sets itself as parent's menu bar
-#  \param style
-#    \b \e int : Menu bar style
 class MenuBar(wx.MenuBar):
+    ## Constructor
+    #
+    #  \param parent
+    #    <b><i>wx.Window</i></b> parent window of the menu bar
+    #    If not None, automatically sets itself as parent's menu bar
+    #  \param style
+    #    Menu bar style represented by an <b><i>integer</i></b> value
     def __init__(self, parent=None, style=0):
         wx.MenuBar.__init__(self, style)
         
@@ -29,25 +30,25 @@ class MenuBar(wx.MenuBar):
     ## Append a menu to the end of menu bar
     #
     #  \param menu
-    #    \b \e wx.Menu : Menu to be appended
+    #    <b><i>wx.Menu</i></b> instance to be appended
     #  \param title
-    #    \b \e unicode|str : Text to be displayed in the menu bar
-    #  \param ID
-    #    \b \e int : ID to store for menu
-    def Append(self, menu, title, ID):
-        self.id_list.append(ID)
+    #    Label to be displayed in the menu bar
+    #  \param menuId
+    #    Unique <b><i>integer</i></b> identifier to store for menu
+    def Append(self, menu, title, menuId):
+        self.id_list.append(menuId)
         
         return wx.MenuBar.Append(self, menu, title)
     
     
     ## Finds a wx.Menu by ID
     #
-    #  \param ID
-    #    \b \e int : ID to search for in menu bar
+    #  \param menuId
+    #    Menu <b><i>integer</i></b> identifier to search for in menu bar
     #  \return
-    #    The wx.Menu with ID
-    def GetMenuById(self, ID):
-        m_index = self.id_list.index(ID)
+    #    The <b><i>wx.Menu</i></b> with using identifier
+    def GetMenuById(self, menuId):
+        m_index = self.id_list.index(menuId)
         
         return self.GetMenu(m_index)
     
@@ -55,14 +56,14 @@ class MenuBar(wx.MenuBar):
     ## Insert a menu to a specified position in the menu bar
     #
     #  \param index
-    #    \b \e int : Position index to insert menu
+    #    Position index to insert menu
     #  \param menu
-    #    \b \e wx.Menu : Menu to be inserted
+    #    <b><i>wx.Menu</i></b> instance to be inserted
     #  \param title
-    #    \b \e unicode|str : Text to be displayed in the menu bar
-    #  \param ID
-    #    \b \e int : ID to store for menu
-    def Insert(self, index, menu, title, ID):
-        self.id_list.insert(index, ID)
+    #    Label to be displayed in the menu bar
+    #  \param menuId
+    #    Unique <b><i>integer</i></b> identifier to store for menu
+    def Insert(self, index, menu, title, menuId):
+        self.id_list.insert(index, menuId)
         
         return wx.MenuBar.Insert(self, index, menu, title)
