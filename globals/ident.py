3d164aed34bb6bfac75633b3ab4bc913c01256d5
AntumDeluge <antumdeluge@gmail.com>
2017-02-10 19:54:06 -0800

    globals.ident: Rename 'BtnId' class to 'ButtonId'

--- globals/ident.py
+++ globals/ident.py
@@ -194,7 +194,7 @@ inputid = InputId()
 
 
 ## IDs for button fields
-class BtnId(FieldId):
+class ButtonId(FieldId):
     def __init__(self):
         FieldId.__init__(self)
         
@@ -268,7 +268,7 @@ class BtnId(FieldId):
         
         return new_id
 
-btnid = BtnId()
+btnid = ButtonId()
 
 
 ## IDs for check box fields
