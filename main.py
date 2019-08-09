cec794ac73a216e02c233536de25f270012cb03b
Jordan Irwin <antumdeluge@gmail.com>
2019-08-09 11:21:32 -0700

    Change manual URL

--- main.py
+++ main.py
@@ -278,7 +278,7 @@ class MainWindow(wx.Frame, ModuleAccessCtrl):
 
 			if status:
 				wx.Yield()
-				webbrowser.open(u'http://debreate.sourceforge.net/usage')
+				webbrowser.open(u'https://debreate.wordpress.com/manual/')
 
 
 	## Opens the logs directory in the system's default file manager
