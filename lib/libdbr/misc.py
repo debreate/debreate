d9dab767687b7121f7b4796a2bd0b6c0adf01017
Jordan Irwin <antumdeluge@gmail.com>
2023-04-02 21:45:26 -0700

    Add function to libdbr.misc to parse latest changes from a changelog file

--- lib/libdbr/misc.py
+++ lib/libdbr/misc.py
@@ -13,6 +13,7 @@ import importlib
 import traceback
 import types
 
+from libdbr        import fileio
 from libdbr.logger import getLogger
 
 
@@ -30,6 +31,32 @@ def generateMD5Hash(data):
   tmp = hashlib.md5()
   tmp.update(data)
   return tmp.hexdigest()
+
+## Parses most recent changes from a changelog-style file.
+#
+#  @param changelog
+#    Path to changelog file.
+#  @return
+#    String containing topmost section of changelog data.
+def getLatestChanges(changelog):
+  data = fileio.readFile(changelog)
+  if not data:
+    return ""
+  changes = []
+  in_changes = False
+  for li in data.split("\n"):
+    if li.strip():
+      in_changes = True
+      changes.append(li)
+    elif in_changes:
+      break
+  if changes:
+    # remove first line if it is a section header
+    header = changes[0].strip()
+    if not header.startswith("-") and not header.startswith("*"):
+      changes.pop(0)
+    return "\n".join(changes)
+  return ""
 
 ## Run a Unit test.
 #
---
+++ tests/testLatestChanges.py
@@ -0,0 +1,19 @@
+
+# ****************************************************
+# * Copyright (C) 2023 - Jordan Irwin (AntumDeluge)  *
+# ****************************************************
+# * This software is licensed under the MIT license. *
+# * See: docs/LICENSE.txt for details.               *
+# ****************************************************
+
+import os
+
+from libdbr      import paths
+from libdbr.misc import getLatestChanges
+
+
+def init():
+  changelog = paths.join(paths.getAppRoot(), "docs/changelog")
+  assert os.path.isfile(changelog)
+  changes = getLatestChanges(changelog)
+  assert changes.strip() != ""
