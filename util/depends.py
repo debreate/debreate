c5c16f0bcef5671b2b361a606fc44f46d4f1ab81
Jordan Irwin <antumdeluge@gmail.com>
2023-04-11 14:05:36 -0700

    Add function to retrieved wxPython version tuple

--- util/depends.py
+++ util/depends.py
@@ -14,13 +14,23 @@ from libdbr.modules import getModule
 
 logger = Logger(__name__)
 
-def checkWx():
+__wx_version = None
+
+def getWxVersion():
+  global wx, __wx_version
   wx = getModule("wx")
+  if __wx_version != None:
+    return __wx_version
+  ver = []
+  for v in wx.__version__.split("."):
+    ver.append(int(v))
+  __wx_version = tuple(ver)
+  return __wx_version
+
+def checkWx():
   wx_min = [4, 0, 7]
   wx_rec = [4, 1, 1]
-  wx_ver = []
-  for v in wx.__version__.split("."):
-    wx_ver.append(int(v))
+  wx_ver = list(getWxVersion())
   if wx_ver < wx_min:
     tmp = ".".join(str(wx_min).strip("[]").split(", "))
     msg = "wxPython minimum required version is {}, found {}".format(tmp, wx.__version__)
