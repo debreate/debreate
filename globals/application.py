f3adf250e4d050ed1593e132e6ed94924cc64e93
Jordan Irwin <antumdeluge@gmail.com>
2023-04-15 07:36:16 -0700

    Increment development version

--- globals/application.py
+++ globals/application.py
@@ -46,7 +46,7 @@ if VERSION_rev > 0:
 VERSION_tuple = tuple(VERSION_tuple)
 
 # Development version: Increment for every development release
-VERSION_dev = 5
+VERSION_dev = 6
 if VERSION_dev:
   VERSION_string = "{}-dev{}".format(VERSION_string, VERSION_dev)
 
